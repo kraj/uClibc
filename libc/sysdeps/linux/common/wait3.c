42010194d04d05f1dea6a30579941ed2ee0273f4
Randolph Chung <tausq@debian.org>
2000-07-09 03:57:13 +0000

    the rest of the compile fixes

--- libc/sysdeps/linux/common/wait3.c
+++ libc/sysdeps/linux/common/wait3.c
@@ -3,9 +3,6 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 
-static inline
-_syscall4(__pid_t,wait4,__pid_t,pid,__WAIT_STATUS,status,int,options,struct rusage *,ru)
-
 __pid_t wait3(__WAIT_STATUS wait_stat, int options, struct rusage *reserved)
 {
 	return wait4((-1) /* WAIT_ANY*/, wait_stat, options, reserved);
--- libc/sysdeps/linux/common/waitpid.c
+++ libc/sysdeps/linux/common/waitpid.c
@@ -3,9 +3,6 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 
-static inline
-_syscall4(__pid_t,wait4,__pid_t,pid,__WAIT_STATUS,status,int,options,struct rusage *,ru)
-
 __pid_t
 waitpid(__pid_t pid, int *wait_stat, int options)
 {
