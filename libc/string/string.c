b540a2898ed9feb95b40e4c35ab3789fb7871b86
Eric Andersen <andersen@codepoet.org>
2001-02-09 08:54:29 +0000

    Add missing ffs, used by rpc crap.  -Erik

--- libc/string/string.c
+++ libc/string/string.c
@@ -280,5 +280,38 @@ int memcmp(const void *s1, const void *s2, size_t len)
 #endif
 
 
+/********************** Function memcmp ************************************/
+
+#ifdef L_ffs
+int ffs(int x)
+{
+	int r = 1;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff)) {
+		x >>= 16;
+		r += 16;
+	}
+	if (!(x & 0xff)) {
+		x >>= 8;
+		r += 8;
+	}
+	if (!(x & 0xf)) {
+		x >>= 4;
+		r += 4;
+	}
+	if (!(x & 3)) {
+		x >>= 2;
+		r += 2;
+	}
+	if (!(x & 1)) {
+		x >>= 1;
+		r += 1;
+	}
+	return r;
+}
+#endif
+
 
 /********************** THE END ********************************************/
