bb6334f6f024ca7f2d66d282e4cd34180f102a3e
Eric Andersen <andersen@codepoet.org>
2001-04-25 16:17:39 +0000

    As pointed out by Michiel Thuys <michiel.thuys@intersil.com>, this was checking sizeof which was pure bloat and in many cases wrong.  -Erik

--- libc/sysdeps/linux/common/seteuid.c
+++ libc/sysdeps/linux/common/seteuid.c
@@ -4,19 +4,5 @@
 
 int seteuid(uid_t uid)
 {
-  switch (sizeof (uid_t))
-  {
-  case 2:
-    if (uid == 65535)
-    {
-      __set_errno(EINVAL);
-      return -1;
-    }
-    break;
-
-  default:
-    fprintf (stderr, "Uknown uid_t size and sign\n");
-  }
-
-  return setreuid(-1, uid);
+    return setreuid(-1, uid);
 }
