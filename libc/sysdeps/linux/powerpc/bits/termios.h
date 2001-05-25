b9f38c7eadb4916d62bbdb688d0321bff3a37413
David Schleef <ds@schleef.org>
2001-05-25 23:25:56 +0000

    Random change to get a log message: Previous change is to make this header more compatible with the kernel and not with glibc.

--- libc/sysdeps/linux/powerpc/bits/termios.h
+++ libc/sysdeps/linux/powerpc/bits/termios.h
@@ -34,7 +34,7 @@ struct termios
     tcflag_t c_cflag;		/* control mode flags */
     tcflag_t c_lflag;		/* local mode flags */
     cc_t c_cc[NCCS];		/* control characters */
-    cc_t c_line;			/* line discipline */
+    cc_t c_line;		/* line discipline */
     speed_t c_ispeed;		/* input speed */
     speed_t c_ospeed;		/* output speed */
   };
