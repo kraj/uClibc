677eb9f98fa6afc485c4c6afcf8b17dcee723f77
Eric Andersen <andersen@codepoet.org>
2001-05-25 01:30:16 +0000

    Update for the SH port from Jean-Yves Avenard <jean-yves_avenard@hp.com>

--- libc/sysdeps/linux/sh/longjmp.c
+++ libc/sysdeps/linux/sh/longjmp.c
@@ -1,31 +1,29 @@
 /* Copyright (C) 1991, 92, 94, 95, 97, 98, 2000 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+   Copyright (C) 2001 Hewlett-Packard Australia
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Library General Public License as published by the Free
+ Software Foundation; either version 2 of the License, or (at your option) any
+ later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+ details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+ You should have received a copy of the GNU Library General Public License
+ along with this program; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Derived in part from the Linux-8086 C library, the GNU C Library, and several
+ other sundry sources.  Files within this library are copyright by their
+ respective copyright holders.
+*/
 
 #include <stddef.h>
 #include <setjmp.h>
 #include <signal.h>
 
-
-#if 0
-extern void _longjmp_unwind (jmp_buf env, int val);
-#endif
-extern void __longjmp(__jmp_buf __env, int __val) 
-	 __attribute__ ((__noreturn__));
 
 /* Set the signal mask to the one specified in ENV, and jump
    to the position specified in ENV, causing the setjmp
@@ -33,11 +31,6 @@ extern void __longjmp(__jmp_buf __env, int __val)
 void
 __uClibc_siglongjmp (sigjmp_buf env, int val)
 {
-#if 0
-  /* Perform any cleanups needed by the frames being unwound.  */
-  _longjmp_unwind (env, val);
-#endif
-
   if (env[0].__mask_was_saved)
     /* Restore the saved signal mask.  */
     (void) __sigprocmask (SIG_SETMASK, &env[0].__saved_mask,
