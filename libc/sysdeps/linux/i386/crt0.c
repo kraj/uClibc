1a5c8c4765bd07f9503282b9db75aa4466016660
Eric Andersen <andersen@codepoet.org>
2001-06-15 19:58:36 +0000

    I am very proud of myself.  I managed to write a working _start for x86 entirely in C -- no asm required.  muahahahaha!  -Erik

---
+++ libc/sysdeps/linux/i386/crt0.c
@@ -0,0 +1,40 @@
+/*
+ * uClibc/sysdeps/linux/i386/crt0.S
+ * Pull stuff off the stack and get uClibc moving.
+ *
+ * Copyright (C) 2001 by Lineo, inc.  
+ * Written by Erik Andersen <andersen@lineo.com>, <andersee@debian.org>
+ * who is very pleased he managed to do this entirely in C code.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+extern void __uClibc_main(int argc,void *argv,void *envp);
+
+void _start(unsigned int first_arg)
+{
+	unsigned int argc;
+	char **argv, **envp;
+	unsigned long *stack;
+
+	stack = (unsigned long*) &first_arg;
+	argc = *(stack - 1);
+	argv = (char **) stack;
+	envp = (char **)stack + argc + 1;
+
+	__uClibc_main(argc, argv, envp);
+}
+
