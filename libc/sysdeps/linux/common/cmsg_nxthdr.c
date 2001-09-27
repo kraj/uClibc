7bd9ab876040ad73c743561c4c966c6ccf419ecf
Eric Andersen <andersen@codepoet.org>
2001-09-27 06:48:46 +0000

    Silence a silly warning

--- libc/sysdeps/linux/common/cmsg_nxthdr.c
+++ libc/sysdeps/linux/common/cmsg_nxthdr.c
@@ -17,6 +17,8 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#define __FORCE_GLIBC__
+#include <features.h>
 #include <sys/socket.h>
 
 
