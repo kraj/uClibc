245b25a3d140d868c5b3442abfeb5e547e6fbd07
Eric Andersen <andersen@codepoet.org>
2001-10-14 04:50:39 +0000

    Erwin Authried <eauth@softsys.co.at> noticed that _XOPEN_SOURCE is supposed to be a number

--- libc/unistd/sysconf.c
+++ libc/unistd/sysconf.c
@@ -16,7 +16,8 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#define _XOPEN_SOURCE
+#define _XOPEN_SOURCE	500
+#include <features.h>
 #include <errno.h>
 #include <limits.h>
 #include <grp.h>
