c374b5e0baa4f7248ab7d0b2dbe8780cc08b899a
Eric Andersen <andersen@codepoet.org>
2001-10-16 04:34:10 +0000

    Oops.  I merged things backwards.  Lets try that again...

--- libc/sysdeps/linux/v850/_mmap.c
+++ libc/sysdeps/linux/v850/_mmap.c
@@ -3,18 +3,6 @@
 #include <sys/mman.h>
 #include <sys/syscall.h>
 
-
-__ptr_t mmap(__ptr_t addr, size_t len, int prot, 
-	int flags, int fd, __off_t offset)
-{
-	unsigned long buffer[6];
-
-	buffer[0] = (unsigned long) addr;
-	buffer[1] = (unsigned long) len;
-	buffer[2] = (unsigned long) prot;
-	buffer[3] = (unsigned long) flags;
-	buffer[4] = (unsigned long) fd;
-	buffer[5] = (unsigned long) offset;
-	return (__ptr_t) _mmap(buffer);
-}
+_syscall6(__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
+	  int, flags, int, fd, __off_t, offset);
 
