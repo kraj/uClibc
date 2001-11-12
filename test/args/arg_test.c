219680fbe34686c2b418da326b4ca6d00a048a64
David Schleef <ds@schleef.org>
2001-11-12 03:10:42 +0000

    Fix common C ambiguity.

--- test/args/arg_test.c
+++ test/args/arg_test.c
@@ -31,8 +31,8 @@ int main( int argc, char **argv)
 	
 	fprintf(stderr, "argc=%d\n", argc);
 
-	while(i<argc) {
-		fprintf(stderr, "argv[%d]='%s'\n", i, argv[i++]);
+	for(i=0;i<argc;i++){
+		fprintf(stderr, "argv[%d]='%s'\n", i, argv[i]);
 	}
 
 	i=0;
