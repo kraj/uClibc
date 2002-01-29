8fda4c4091b05523123b9a59f627a08581e73c65
Eric Andersen <andersen@codepoet.org>
2002-01-29 09:02:55 +0000

    Updates to the v850 architecture from Miles Bader <miles@gnu.org>

--- include/stdio.h
+++ include/stdio.h
@@ -1,5 +1,5 @@
 /* Define ISO C stdio on top of C++ iostreams.
-   Copyright (C) 1991, 1994-1999, 2000, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1991, 1994-1999, 2000, 2001, 2002 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
