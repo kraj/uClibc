a6cf8dc644239941c5f4a90fd5408479efe50617
Eric Andersen <andersen@codepoet.org>
2002-01-30 07:11:08 +0000

    Fixup setjmp implementation so it actaully works as expected  -Erik

--- libc/signal/sigjmp.c
+++ libc/signal/sigjmp.c
@@ -2,19 +2,19 @@
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 #include <stddef.h>
 #include <setjmp.h>
@@ -24,12 +24,10 @@
    before doing a `__setjmp' on ENV[0].__jmpbuf.
    Always return zero.  */
 
-int
-__sigjmp_save (sigjmp_buf env, int savemask)
+int __sigjmp_save (sigjmp_buf env, int savemask)
 {
-  env[0].__mask_was_saved = (savemask &&
-			     sigprocmask (SIG_BLOCK, (sigset_t *) NULL,
-					    &env[0].__saved_mask) == 0);
+    env[0].__mask_was_saved = (savemask && 
+	    sigprocmask (SIG_BLOCK, (sigset_t *) NULL, &env[0].__saved_mask) == 0);
 
-  return 0;
+    return 0;
 }
