85750cfa27211c684a5c02810d4970818d8cd05e
Eric Andersen <andersen@codepoet.org>
2002-02-20 09:50:24 +0000

    This is a bit less messy

--- libc/misc/internals/errno.c
+++ libc/misc/internals/errno.c
@@ -1,29 +1,14 @@
-#if 0
 #include <features.h>
-/* Unforunately, this produces noisy warnings... */
-int errno __attribute__ ((section  (".bss")));
-int h_errno __attribute__ ((section  (".bss")));
-weak_alias(errno, _errno);
-weak_alias(h_errno, _h_errno);
+#undef errno
+
+#if 0
+/* Unfortunately, this doesn't work... */
+int h_errno __attribute__ ((section  (".bss"))) = 0;
+int errno __attribute__ ((section  (".bss"))) = 0;
 #else
-__asm__("
-.weak _errno;
-_errno = errno
-.weak _h_errno;
-_h_errno = h_errno
-
-.bss
-.globl  errno
-.type errno,%object
-.size errno,4
-errno:
-    .space  4
-
-.bss
-.globl  h_errno
-.type h_errno,%object
-.size h_errno,4
-h_errno:
-    .space  4
-");
+int errno = 0;
+int h_errno = 0;
 #endif
+
+weak_alias (errno, _errno)
+weak_alias(h_errno, _h_errno);
