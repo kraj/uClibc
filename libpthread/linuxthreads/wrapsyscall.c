a8021309c2cb8c1e65ba2d624e3b8ed3995fc674
Eric Andersen <andersen@codepoet.org>
2002-02-21 22:49:04 +0000

    Let the large file stuff be cancelable

--- libpthread/linuxthreads/wrapsyscall.c
+++ libpthread/linuxthreads/wrapsyscall.c
@@ -1,5 +1,5 @@
 /* Wrapper arpund system calls to provide cancelation points.
-   Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1996,1997,1998,1999,2000,2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
@@ -43,6 +43,7 @@ const int __pthread_provide_wrappers = 0;
 #define CANCELABLE_SYSCALL(res_type, name, param_list, params) \
 res_type __libc_##name param_list;					      \
 res_type								      \
+__attribute__ ((weak))							      \
 name param_list								      \
 {									      \
   res_type result;							      \
@@ -56,6 +57,7 @@ name param_list								      \
 #define CANCELABLE_SYSCALL_VA(res_type, name, param_list, params, last_arg) \
 res_type __libc_##name param_list;					      \
 res_type								      \
+__attribute__ ((weak))							      \
 name param_list								      \
 {									      \
   res_type result;							      \
@@ -72,11 +74,13 @@ name param_list								      \
 
 /* close(2).  */
 CANCELABLE_SYSCALL (int, close, (int fd), (fd))
+strong_alias (close, __close)
 
 
 /* fcntl(2).  */
 CANCELABLE_SYSCALL_VA (int, fcntl, (int fd, int cmd, ...),
 		       (fd, cmd, va_arg (ap, long int)), cmd)
+strong_alias (fcntl, __fcntl)
 
 
 /* fsync(2).  */
@@ -86,13 +90,18 @@ CANCELABLE_SYSCALL (int, fsync, (int fd), (fd))
 /* lseek(2).  */
 CANCELABLE_SYSCALL (off_t, lseek, (int fd, off_t offset, int whence),
 		    (fd, offset, whence))
+strong_alias (lseek, __lseek)
 
+#ifdef __UCLIBC_HAVE_LFS__
+/* lseek64(2).  */
+CANCELABLE_SYSCALL (off64_t, lseek64, (int fd, off64_t offset, int whence),
+		    (fd, offset, whence))
+#endif
 
 /* msync(2).  */
-/* This syscall not implemented in uClibc
 CANCELABLE_SYSCALL (int, msync, (__ptr_t addr, size_t length, int flags),
 		    (addr, length, flags))
-*/
+
 
 /* nanosleep(2).  */
 CANCELABLE_SYSCALL (int, nanosleep, (const struct timespec *requested_time,
@@ -103,25 +112,65 @@ CANCELABLE_SYSCALL (int, nanosleep, (const struct timespec *requested_time,
 /* open(2).  */
 CANCELABLE_SYSCALL_VA (int, open, (const char *pathname, int flags, ...),
 		       (pathname, flags, va_arg (ap, mode_t)), flags)
+strong_alias (open, __open)
 
+
+#ifdef __UCLIBC_HAVE_LFS__
+/* open64(3).  */
+CANCELABLE_SYSCALL_VA (int, open64, (const char *pathname, int flags, ...),
+		       (pathname, flags, va_arg (ap, mode_t)), flags)
+strong_alias (open64, __open64)
+#endif
 
 /* pause(2).  */
 CANCELABLE_SYSCALL (int, pause, (void), ())
 
 
+/* pread(3).  */
+CANCELABLE_SYSCALL (ssize_t, pread, (int fd, void *buf, size_t count,
+				     off_t offset),
+		    (fd, buf, count, offset))
+
+
+#ifdef __UCLIBC_HAVE_LFS__
+/* pread64(3).  */
+CANCELABLE_SYSCALL (ssize_t, pread64, (int fd, void *buf, size_t count,
+				       off64_t offset),
+		    (fd, buf, count, offset))
+strong_alias (pread64, __pread64)
+#endif
+
+/* pwrite(3).  */
+CANCELABLE_SYSCALL (ssize_t, pwrite, (int fd, const void *buf, size_t n,
+				      off_t offset),
+		    (fd, buf, n, offset))
+
+
+#ifdef __UCLIBC_HAVE_LFS__
+/* pwrite64(3).  */
+CANCELABLE_SYSCALL (ssize_t, pwrite64, (int fd, const void *buf, size_t n,
+					off64_t offset),
+		    (fd, buf, n, offset))
+strong_alias (pwrite64, __pwrite64)
+#endif
+
 /* read(2).  */
 CANCELABLE_SYSCALL (ssize_t, read, (int fd, void *buf, size_t count),
 		    (fd, buf, count))
+strong_alias (read, __read)
 
 
 /* system(3).  */
 CANCELABLE_SYSCALL (int, system, (const char *line), (line))
 
+
 /* tcdrain(2).  */
 CANCELABLE_SYSCALL (int, tcdrain, (int fd), (fd))
 
+
 /* wait(2).  */
 CANCELABLE_SYSCALL (__pid_t, wait, (__WAIT_STATUS_DEFN stat_loc), (stat_loc))
+strong_alias (wait, __wait)
 
 
 /* waitpid(2).  */
@@ -133,6 +182,7 @@ CANCELABLE_SYSCALL (__pid_t, waitpid, (__pid_t pid, int *stat_loc,
 /* write(2).  */
 CANCELABLE_SYSCALL (ssize_t, write, (int fd, const void *buf, size_t n),
 		    (fd, buf, n))
+strong_alias (write, __write)
 
 
 /* The following system calls are thread cancellation points specified
@@ -147,32 +197,34 @@ CANCELABLE_SYSCALL (int, accept, (int fd, __SOCKADDR_ARG addr,
 CANCELABLE_SYSCALL (int, connect, (int fd, __CONST_SOCKADDR_ARG addr,
 				     socklen_t len),
 		    (fd, addr, len))
+strong_alias (connect, __connect)
 
 /* recv(2).  */
-CANCELABLE_SYSCALL (int, recv, (int fd, __ptr_t buf, size_t n, int flags),
+CANCELABLE_SYSCALL (ssize_t, recv, (int fd, __ptr_t buf, size_t n, int flags),
 		    (fd, buf, n, flags))
 
 /* recvfrom(2).  */
-CANCELABLE_SYSCALL (int, recvfrom, (int fd, __ptr_t buf, size_t n, int flags,
-				    __SOCKADDR_ARG addr, socklen_t *addr_len),
+CANCELABLE_SYSCALL (ssize_t, recvfrom, (int fd, __ptr_t buf, size_t n, int flags,
+					__SOCKADDR_ARG addr, socklen_t *addr_len),
 		    (fd, buf, n, flags, addr, addr_len))
 
 /* recvmsg(2).  */
-CANCELABLE_SYSCALL (int, recvmsg, (int fd, struct msghdr *message, int flags),
+CANCELABLE_SYSCALL (ssize_t, recvmsg, (int fd, struct msghdr *message, int flags),
 		    (fd, message, flags))
 
 /* send(2).  */
-CANCELABLE_SYSCALL (int, send, (int fd, const __ptr_t buf, size_t n,
-				int flags),
+CANCELABLE_SYSCALL (ssize_t, send, (int fd, const __ptr_t buf, size_t n,
+				    int flags),
 		    (fd, buf, n, flags))
+strong_alias (send, __send)
 
 /* sendmsg(2).  */
-CANCELABLE_SYSCALL (int, sendmsg, (int fd, const struct msghdr *message,
-				   int flags),
+CANCELABLE_SYSCALL (ssize_t, sendmsg, (int fd, const struct msghdr *message,
+				       int flags),
 		    (fd, message, flags))
 
 /* sendto(2).  */
-CANCELABLE_SYSCALL (int, sendto, (int fd, const __ptr_t buf, size_t n,
-				  int flags, __CONST_SOCKADDR_ARG addr,
-				  socklen_t addr_len),
+CANCELABLE_SYSCALL (ssize_t, sendto, (int fd, const __ptr_t buf, size_t n,
+				      int flags, __CONST_SOCKADDR_ARG addr,
+				      socklen_t addr_len),
 		    (fd, buf, n, flags, addr, addr_len))
