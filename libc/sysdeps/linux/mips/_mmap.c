5640182e962a3190dd28eff714cdda9de2d3f877
Eric Andersen <andersen@codepoet.org>
2002-02-22 00:10:00 +0000

    Several mips fixes from Geoffrey Espin.  With these, busybox with ash/vi/etc now works just fine with uClibc on mips.

---
+++ libc/sysdeps/linux/mips/_mmap.c
@@ -0,0 +1,8 @@
+/* Use new style mmap for mips */
+#include <unistd.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+
+_syscall6 (__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
+	   int, flags, int, fd, __off_t, offset);
