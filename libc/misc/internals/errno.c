58c542d570047fdbf72deaadeb786d6a5a04bc4d
Eric Andersen <andersen@codepoet.org>
2002-02-25 13:00:39 +0000

    Try this version instead...

--- libc/misc/internals/errno.c
+++ libc/misc/internals/errno.c
@@ -1,14 +1,17 @@
 #include <features.h>
 #undef errno
 
+extern int errno;
+extern int h_errno;
+
 #if 0
 /* Unfortunately, this doesn't work... */
 int h_errno __attribute__ ((section  (".bss"))) = 0;
 int errno __attribute__ ((section  (".bss"))) = 0;
 #else
-int errno = 0;
-int h_errno = 0;
+int _errno = 0;
+int _h_errno = 0;
 #endif
 
-weak_alias (errno, _errno)
-weak_alias(h_errno, _h_errno);
+weak_alias (_errno, errno)
+weak_alias(_h_errno, h_errno);
