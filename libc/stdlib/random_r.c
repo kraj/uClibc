e2d6080d4d663c4c8bee9df1d1b6a87fa1944a22
Eric Andersen <andersen@codepoet.org>
2002-03-19 12:19:07 +0000

    Merge glibc random, which gets us a much better RNG and a reentrant one as well.  It is not much bigger than what we had, so...  -Erik

--- libc/stdlib/random_r.c
+++ libc/stdlib/random_r.c
@@ -19,24 +19,17 @@
  * This is derived from the Berkeley source:
  *	@(#)random.c	5.5 (Berkeley) 7/6/88
  * It was reworked for the GNU C Library by Roland McGrath.
- * Rewritten to use reentrant functions by Ulrich Drepper, 1995.
+ * Rewritten to be reentrant by Ulrich Drepper, 1995
  */
 
 #define _GNU_SOURCE
 #include <features.h>
+#include <errno.h>
 #include <limits.h>
 #include <stddef.h>
 #include <stdlib.h>
-#ifdef __UCLIBC_HAS_THREADS__
-#include <pthread.h>
-/* POSIX.1c requires that there is mutual exclusion for the `rand' and
-   `srand' functions to prevent concurrent calls from modifying common
-   data.  */
-static pthread_mutex_t lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-#else
-#define pthread_mutex_lock(x)
-#define pthread_mutex_unlock(x)
-#endif
+
+
 
 /* An improved random number generation package.  In addition to the standard
    rand()/srand() like interface, this package also has a special state info
@@ -115,65 +108,21 @@ static pthread_mutex_t lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
 
 #define	MAX_TYPES	5	/* Max number of types above.  */
 
-
-/* Initially, everything is set up as if from:
-	initstate(1, randtbl, 128);
-   Note that this initialization takes advantage of the fact that srandom
-   advances the front and rear pointers 10*rand_deg times, and hence the
-   rear pointer which starts at 0 will also end up at zero; thus the zeroth
-   element of the state information, which contains info about the current
-   position of the rear pointer is just
-	(MAX_TYPES * (rptr - state)) + TYPE_3 == TYPE_3.  */
-
-static int32_t randtbl[DEG_3 + 1] =
+struct random_poly_info
 {
-    TYPE_3,
+    int seps[MAX_TYPES];
+    int degrees[MAX_TYPES];
+};
 
-    -1726662223, 379960547, 1735697613, 1040273694, 1313901226,
-    1627687941, -179304937, -2073333483, 1780058412, -1989503057,
-    -615974602, 344556628, 939512070, -1249116260, 1507946756,
-    -812545463, 154635395, 1388815473, -1926676823, 525320961,
-    -1009028674, 968117788, -123449607, 1284210865, 435012392,
-    -2017506339, -911064859, -370259173, 1132637927, 1398500161,
-    -205601318,
+static const struct random_poly_info random_poly_info =
+{
+    { SEP_0, SEP_1, SEP_2, SEP_3, SEP_4 },
+    { DEG_0, DEG_1, DEG_2, DEG_3, DEG_4 }
 };
 
 
-static struct random_data unsafe_state =
-{
-    /* FPTR and RPTR are two pointers into the state info, a front and a rear
-       pointer.  These two pointers are always rand_sep places aparts, as they
-       cycle through the state information.  (Yes, this does mean we could get
-       away with just one pointer, but the code for random is more efficient
-       this way).  The pointers are left positioned as they would be from the call:
-       initstate(1, randtbl, 128);
-       (The position of the rear pointer, rptr, is really 0 (as explained above
-       in the initialization of randtbl) because the state table pointer is set
-       to point to randtbl[1] (as explained below).)  */
 
-    fptr : &randtbl[SEP_3 + 1],
-    rptr : &randtbl[1],
-
-    /* The following things are the pointer to the state information table,
-       the type of the current generator, the degree of the current polynomial
-       being used, and the separation between the two pointers.
-       Note that for efficiency of random, we remember the first location of
-       the state information, not the zeroth.  Hence it is valid to access
-       state[-1], which is used to store the type of the R.N.G.
-       Also, we remember the last location, since this is more efficient than
-       indexing every time to find the address of the last element to see if
-       the front and rear pointers have wrapped.  */
-
-    state : &randtbl[1],
-
-    rand_type : TYPE_3,
-    rand_deg : DEG_3,
-    rand_sep : SEP_3,
-
-    end_ptr : &randtbl[sizeof (randtbl) / sizeof (randtbl[0])]
-};
-
-
+
 /* Initialize the random number generator based on the given seed.  If the
    type is the trivial no-state-information type, just remember the seed.
    Otherwise, initializes state[] based on the given "seed" via a linear
@@ -182,13 +131,60 @@ static struct random_data unsafe_state =
    information a given number of times to get rid of any initial dependencies
    introduced by the L.C.R.N.G.  Note that the initialization of randtbl[]
    for default usage relies on values produced by this routine.  */
-void srandom (unsigned int x)
+int srandom_r (unsigned int seed, struct random_data *buf)
 {
-    pthread_mutex_lock(&lock);
-    srandom_r (x, &unsafe_state);
-    pthread_mutex_unlock(&lock);
+    int type;
+    int32_t *state;
+    long int i;
+    long int word;
+    int32_t *dst;
+    int kc;
+
+    if (buf == NULL)
+	goto fail;
+    type = buf->rand_type;
+    if ((unsigned int) type >= MAX_TYPES)
+	goto fail;
+
+    state = buf->state;
+    /* We must make sure the seed is not 0.  Take arbitrarily 1 in this case.  */
+    if (seed == 0)
+	seed = 1;
+    state[0] = seed;
+    if (type == TYPE_0)
+	goto done;
+
+    dst = state;
+    word = seed;
+    kc = buf->rand_deg;
+    for (i = 1; i < kc; ++i)
+    {
+	/* This does:
+	   state[i] = (16807 * state[i - 1]) % 2147483647;
+	   but avoids overflowing 31 bits.  */
+	long int hi = word / 127773;
+	long int lo = word % 127773;
+	word = 16807 * lo - 2836 * hi;
+	if (word < 0)
+	    word += 2147483647;
+	*++dst = word;
+    }
+
+    buf->fptr = &state[buf->rand_sep];
+    buf->rptr = &state[0];
+    kc *= 10;
+    while (--kc >= 0)
+    {
+	int32_t discard;
+	(void) random_r (buf, &discard);
+    }
+
+done:
+    return 0;
+
+fail:
+    return -1;
 }
-weak_alias (srandom, srand)
 
 /* Initialize the state information in the given array of N bytes for
    future random number generation.  Based on the number of bytes we
@@ -201,15 +197,57 @@ weak_alias (srandom, srand)
    Note: The first thing we do is save the current state, if any, just like
    setstate so that it doesn't matter when initstate is called.
    Returns a pointer to the old state.  */
-char * initstate (unsigned int seed, char *arg_state, size_t n)
+int initstate_r (seed, arg_state, n, buf)
+     unsigned int seed;
+     char *arg_state;
+     size_t n;
+     struct random_data *buf;
 {
-    int32_t *ostate;
+    int type;
+    int degree;
+    int separation;
+    int32_t *state;
 
-    pthread_mutex_lock(&lock);
-    ostate = &unsafe_state.state[-1];
-    initstate_r (seed, arg_state, n, &unsafe_state);
-    pthread_mutex_unlock(&lock);
-    return (char *) ostate;
+    if (buf == NULL)
+	goto fail;
+
+    if (n >= BREAK_3)
+	type = n < BREAK_4 ? TYPE_3 : TYPE_4;
+    else if (n < BREAK_1)
+    {
+	if (n < BREAK_0)
+	{
+	    __set_errno (EINVAL);
+	    goto fail;
+	}
+	type = TYPE_0;
+    }
+    else
+	type = n < BREAK_2 ? TYPE_1 : TYPE_2;
+
+    degree = random_poly_info.degrees[type];
+    separation = random_poly_info.seps[type];
+
+    buf->rand_type = type;
+    buf->rand_sep = separation;
+    buf->rand_deg = degree;
+    state = &((int32_t *) arg_state)[1];	/* First location.  */
+    /* Must set END_PTR before srandom.  */
+    buf->end_ptr = &state[degree];
+
+    buf->state = state;
+
+    srandom_r (seed, buf);
+
+    state[-1] = TYPE_0;
+    if (type != TYPE_0)
+	state[-1] = (buf->rptr - state) * MAX_TYPES + type;
+
+    return 0;
+
+fail:
+    __set_errno (EINVAL);
+    return -1;
 }
 
 /* Restore the state from the given state array.
@@ -220,16 +258,48 @@ char * initstate (unsigned int seed, char *arg_state, size_t n)
    to the order in which things are done, it is OK to call setstate with the
    same state as the current state
    Returns a pointer to the old state information.  */
-char * setstate (char *arg_state)
+int setstate_r (char *arg_state, struct random_data *buf)
 {
-    int32_t *ostate;
+    int32_t *new_state = 1 + (int32_t *) arg_state;
+    int type;
+    int old_type;
+    int32_t *old_state;
+    int degree;
+    int separation;
 
-    pthread_mutex_lock(&lock);
-    ostate = &unsafe_state.state[-1];
-    if (setstate_r (arg_state, &unsafe_state) < 0)
-	ostate = NULL;
-    pthread_mutex_unlock(&lock);
-    return (char *) ostate;
+    if (arg_state == NULL || buf == NULL)
+	goto fail;
+
+    old_type = buf->rand_type;
+    old_state = buf->state;
+    if (old_type == TYPE_0)
+	old_state[-1] = TYPE_0;
+    else
+	old_state[-1] = (MAX_TYPES * (buf->rptr - old_state)) + old_type;
+
+    type = new_state[-1] % MAX_TYPES;
+    if (type < TYPE_0 || type > TYPE_4)
+	goto fail;
+
+    buf->rand_deg = degree = random_poly_info.degrees[type];
+    buf->rand_sep = separation = random_poly_info.seps[type];
+    buf->rand_type = type;
+
+    if (type != TYPE_0)
+    {
+	int rear = new_state[-1] / MAX_TYPES;
+	buf->rptr = &new_state[rear];
+	buf->fptr = &new_state[(rear + separation) % degree];
+    }
+    buf->state = new_state;
+    /* Set end_ptr too.  */
+    buf->end_ptr = &new_state[degree];
+
+    return 0;
+
+fail:
+    __set_errno (EINVAL);
+    return -1;
 }
 
 /* If we are using the trivial TYPE_0 R.N.G., just do the old linear
@@ -243,13 +313,53 @@ char * setstate (char *arg_state)
    rear pointers can't wrap on the same call by not testing the rear
    pointer if the front one has wrapped.  Returns a 31-bit random number.  */
 
-long int random ()
+int random_r (buf, result)
+     struct random_data *buf;
+     int32_t *result;
 {
-  int32_t retval;
+    int32_t *state;
 
-  pthread_mutex_lock(&lock);
-  random_r (&unsafe_state, &retval);
-  pthread_mutex_unlock(&lock);
-  return retval;
+    if (buf == NULL || result == NULL)
+	goto fail;
+
+    state = buf->state;
+
+    if (buf->rand_type == TYPE_0)
+    {
+	int32_t val = state[0];
+	val = ((state[0] * 1103515245) + 12345) & 0x7fffffff;
+	state[0] = val;
+	*result = val;
+    }
+    else
+    {
+	int32_t *fptr = buf->fptr;
+	int32_t *rptr = buf->rptr;
+	int32_t *end_ptr = buf->end_ptr;
+	int32_t val;
+
+	val = *fptr += *rptr;
+	/* Chucking least random bit.  */
+	*result = (val >> 1) & 0x7fffffff;
+	++fptr;
+	if (fptr >= end_ptr)
+	{
+	    fptr = state;
+	    ++rptr;
+	}
+	else
+	{
+	    ++rptr;
+	    if (rptr >= end_ptr)
+		rptr = state;
+	}
+	buf->fptr = fptr;
+	buf->rptr = rptr;
+    }
+    return 0;
+
+fail:
+    __set_errno (EINVAL);
+    return -1;
 }
 
