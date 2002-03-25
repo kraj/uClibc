4920e52ebf10a97da801be34816b7d00054e5716
Eric Andersen <andersen@codepoet.org>
2002-03-25 14:29:38 +0000

    Include our own copy of struct sysinfo to avoid binary compatibility problems with Linux 2.4, which changed things.  And thereby reduce by one more the kernel headers we depend on...  -Erik

--- include/sys/sysinfo.h
+++ include/sys/sysinfo.h
@@ -21,8 +21,24 @@
 
 #include <features.h>
 
-/* Get sysinfo structure from kernel header.  */
-#include <linux/kernel.h>
+/* Include our own copy of struct sysinfo to avoid binary compatability
+ * problems with Linux 2.4, which changed things.  Grumble, grumble. */
+struct sysinfo {
+	long uptime;			/* Seconds since boot */
+	unsigned long loads[3];		/* 1, 5, and 15 minute load averages */
+	unsigned long totalram;		/* Total usable main memory size */
+	unsigned long freeram;		/* Available memory size */
+	unsigned long sharedram;	/* Amount of shared memory */
+	unsigned long bufferram;	/* Memory used by buffers */
+	unsigned long totalswap;	/* Total swap space size */
+	unsigned long freeswap;		/* swap space still available */
+	unsigned short procs;		/* Number of current processes */
+	unsigned short pad;			/* Padding needed for m68k */
+	unsigned long totalhigh;	/* Total high memory size */
+	unsigned long freehigh;		/* Available high memory size */
+	unsigned int mem_unit;		/* Memory unit size in bytes */
+	char _f[20-2*sizeof(long)-sizeof(int)];	/* Padding: libc5 uses this.. */
+};
 
 __BEGIN_DECLS
 
