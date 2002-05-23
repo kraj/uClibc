50566667587e92dcf900e2d53204d972b5650a38
Eric Andersen <andersen@codepoet.org>
2002-05-23 19:09:03 +0000

    Several h8300 updates from Yoshinori Sato <qzb04471@nifty.ne.jp>

--- include/features.h
+++ include/features.h
@@ -380,11 +380,17 @@ uClibc was built without large file support enabled.
 	asm (".section "  ".gnu.warning." #symbol  "\n\t.previous");	      \
 	    static const char __evoke_link_warning_##symbol[]		      \
 	    __attribute__ ((section (".gnu.warning." #symbol "\n\t#"))) = msg;
-#else
+#else /* !defined HAVE_ELF */
+#  if defined(__H8300H__)
+#    define SYMBOL_PREFIX "_"      /* H8/300 Target of add "_" to symbol prefix */
+#  else
+#    define SYMBOL_PREFIX 
+#  endif
 #  define strong_alias(name, aliasname) _strong_alias (name, aliasname)
 #  define weak_alias(name, aliasname) _strong_alias (name, aliasname)
 #  define _strong_alias(name, aliasname) \
-	__asm__(".global " #aliasname "\n.set " #aliasname "," #name);
+	__asm__(".global " SYMBOL_PREFIX #aliasname "\n" \
+                ".set " SYMBOL_PREFIX #aliasname "," SYMBOL_PREFIX #name);
 #  define link_warning(symbol, msg) \
 	asm (".stabs \"" msg "\",30,0,0,0\n\t" \
 	      ".stabs \"" #symbol "\",1,0,0,0\n");
