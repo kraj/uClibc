398f1b156bb58ee096ab64c1d8fdeb295833e5d5
Eric Andersen <andersen@codepoet.org>
2002-06-20 14:35:19 +0000

    Add support for doing the mmu-less thing  -Erik

--- extra/gcc-uClibc/gcc-uClibc.c
+++ extra/gcc-uClibc/gcc-uClibc.c
@@ -322,6 +322,9 @@ int main(int argc, char **argv)
 	}
 
 	if (linking && source_count) {
+#ifndef HAS_MMU
+	    gcc_argv[i++] = "-Wl,-elf2flt";
+#endif
 	    if (use_stdlib) {
 		gcc_argv[i++] = nostdlib;
 	    }
