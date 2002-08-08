183799fd6eb753239dccb0ace66412ed9c19247a
Eric Andersen <andersen@codepoet.org>
2002-08-08 15:47:22 +0000

    Add a simple test for shared lib ctor/dtors  -Erik

--- test/ldso/howdy.c
+++ test/ldso/howdy.c
@@ -5,3 +5,15 @@ int howdy(const char *s)
 	return printf("howdy: %s\n", s);
 }
 
+void __attribute__((constructor)) howdy_ctor(void)
+{
+	printf("I am the libhowdy constructor!\n");
+}
+
+void __attribute__((destructor)) howdy_dtor(void)
+{
+	printf("I am the libhowdy destructor!\n");
+}
+
+
+
