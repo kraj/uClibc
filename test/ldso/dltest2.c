c8b3ccaedb7e08a2d25ce4485ff7e13ddc6e92a5
Eric Andersen <andersen@codepoet.org>
2002-08-19 10:01:33 +0000

    Open a lib that is not so closely coupled with gdb

--- test/ldso/dltest2.c
+++ test/ldso/dltest2.c
@@ -10,16 +10,16 @@ int main(int argc, char **argv) {
 	void *handle;
 	td_err_e (*td_init_p) (void);
 
-	fprintf(stderr, "Attempting to dlopen() libthread_db.so with RTLD_NOW\n");
-	handle = dlopen ("libthread_db.so", RTLD_NOW);
+	fprintf(stderr, "Attempting to dlopen() libpthread.so with RTLD_NOW\n");
+	handle = dlopen ("libpthread.so", RTLD_NOW);
 	if (!handle) {
 		fputs (dlerror(), stderr);
 		exit(1);
 	}
 
-	td_init_p = dlsym (handle, "td_init");
+	td_init_p = dlsym (handle, "__pthread_initialize");
 	if (td_init_p == NULL) {
-	    fprintf(stderr, "yipe!  td_init() failed!\n");
+	    fprintf(stderr, "yipe!  __pthread_initialize() failed!\n");
 	    return EXIT_FAILURE;
 	}
 #if 0 //def __UCLIBC__
@@ -28,16 +28,16 @@ int main(int argc, char **argv) {
 	dlclose(handle);
 
 
-	fprintf(stderr, "Attempting to dlopen() libthread_db.so with RTLD_LAZY\n");
-	handle = dlopen ("libthread_db.so", RTLD_LAZY);
+	fprintf(stderr, "Attempting to dlopen() libpthread.so with RTLD_LAZY\n");
+	handle = dlopen ("libpthread.so", RTLD_LAZY);
 	if (!handle) {
 		fputs (dlerror(), stderr);
 		exit(1);
 	}
 
-	td_init_p = dlsym (handle, "td_init");
+	td_init_p = dlsym (handle, "__pthread_initialize");
 	if (td_init_p == NULL) {
-	    fprintf(stderr, "yipe!  td_init() failed!");
+	    fprintf(stderr, "yipe!  __pthread_initialize() failed!");
 	    return EXIT_FAILURE;
 	}
 #if 0 //def __UCLIBC__
