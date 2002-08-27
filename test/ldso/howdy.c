a74fc341b922f38161d10a9470b63ad537e13f07
Eric Andersen <andersen@codepoet.org>
2002-08-27 19:38:14 +0000

    Semonstrate a problem where weak symbols referenced in shared libs are not being overridden when those same symbols _are_ overridden by other shared libs in the main app.  Ick.  -Erik

--- test/ldso/howdy.c
+++ test/ldso/howdy.c
@@ -1,18 +1,23 @@
+#include <pthread.h>
 #include <stdio.h>
+
+extern int __pthread_return_0 (void);
 
 int howdy(const char *s)
 {
-	return printf("howdy: %s\n", s);
+    return printf("howdy:  __pthread_return_0 = %p\n"
+		  "howdy: pthread_cond_signal = %p\n",
+		  __pthread_return_0, pthread_cond_signal);
 }
 
 void __attribute__((constructor)) howdy_ctor(void)
 {
-	printf("I am the libhowdy constructor!\n");
+    printf("I am the libhowdy constructor!\n");
 }
 
 void __attribute__((destructor)) howdy_dtor(void)
 {
-	printf("I am the libhowdy destructor!\n");
+    printf("I am the libhowdy destructor!\n");
 }
 
 
