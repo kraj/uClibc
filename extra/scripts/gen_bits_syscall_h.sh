3a0eeecab4d4c298f14bfb0f63519353ea900ad8
Miles Bader <miles@lsi.nec.co.jp>
2002-10-28 08:33:08 +0000

    Add appropriate -I options so that any files included by unistd.h are   found correctly. Tweak the whitespace of the generated file.

--- extra/scripts/gen_bits_syscall_h.sh
+++ extra/scripts/gen_bits_syscall_h.sh
@@ -9,20 +9,23 @@
 # Warning!!! This does _no_ error checking!!!
 
 UNISTD_H_PATH=$TOPDIR/include/asm/unistd.h
+INCLUDE_OPTS="-I$TOPDIR/include"
 
 ( echo "#include \"$UNISTD_H_PATH\"" ;
-  $CC -E -dN $UNISTD_H_PATH | # needed to strip out any kernel-internal defines
+  $CC -E -dN $INCLUDE_OPTS $UNISTD_H_PATH | # needed to strip out any kernel-internal defines
   sed -ne 's/^[ ]*#define[ ]*__NR_\([A-Za-z0-9_]*\).*/UCLIBC_\1 __NR_\1/gp'
 ) |
-$CC -E - |
+$CC -E $INCLUDE_OPTS - |
 ( echo "/* WARNING!!! AUTO-GENERATED FILE!!! DO NOT EDIT!!! */" ; echo ;
   echo "#ifndef _BITS_SYSNUM_H" ;
   echo "#define _BITS_SYSNUM_H" ;
+  echo ;
   echo "#ifndef _SYSCALL_H" ;
   echo "# error \"Never use <bits/sysnum.h> directly; include <sys/syscall.h> instead.\"" ;
   echo "#endif" ; echo ;
   sed -ne 's/^UCLIBC_\([A-Za-z0-9_]*\) *\(.*\)/#undef __NR_\1\
 #define __NR_\1 \2\
 #define SYS_\1 __NR_\1/gp'
-  echo "#endif" ; echo ;
+  echo ;
+  echo "#endif" ;
 )
