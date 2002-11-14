4c316e6c2c8d44bfdda55fa6bae8efdd345eb14b
Eric Andersen <andersen@codepoet.org>
2002-11-14 00:53:49 +0000

    Add missing quotes  -Erik

--- ldso/ldso/arm/boot1_arch.h
+++ ldso/ldso/arm/boot1_arch.h
@@ -5,19 +5,18 @@
 /* Overrive the default _dl_boot function, and replace it with a bit of asm.
  * Then call the real _dl_boot function, which is now named _dl_boot2. */
 
-asm("\
-.text
-.globl _dl_boot
-_dl_boot:
-	mov     r7, sp
-@	ldr     r0, [sp], #4
-	mov     r0,sp
-	bl      _dl_boot2
-	mov     r6, r0
-	mov     r0, r7
-	mov     pc, r6
-\n\
-");
+asm("" \
+"	.text\n"			\
+"	.globl	_dl_boot\n"		\
+"_dl_boot:\n"				\
+"	mov	r7, sp\n"		\
+"	@ldr	r0, [sp], #4\n"		\
+"	mov	r0, sp\n"		\
+"	bl	_dl_boot2\n"		\
+"	mov	r6, r0\n"		\
+"	mov	r0, r7\n"		\
+"	mov	pc, r6\n"		\
+);
 
 #define _dl_boot _dl_boot2
 #define LD_BOOT(X)   static void *  __attribute__ ((unused)) _dl_boot (X)
