5faa9568e41b08fa5be573662b21ecbe031f612d
Eric Andersen <andersen@codepoet.org>
2003-01-18 19:46:17 +0000

    Only include lockf64 when large file support is enabled, fixing a problem noticed by Jeff Mock.  Sorry about that.  -Erik

--- libc/misc/file/lockf64.c
+++ libc/misc/file/lockf64.c
@@ -16,6 +16,23 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <features.h>
+
+#ifdef __UCLIBC_HAS_LFS__
+#if defined _FILE_OFFSET_BITS && _FILE_OFFSET_BITS != 64 
+#undef _FILE_OFFSET_BITS
+#define	_FILE_OFFSET_BITS   64
+#endif
+#ifndef __USE_FILE_OFFSET64
+# define __USE_FILE_OFFSET64	1
+#endif
+#ifndef __USE_LARGEFILE64
+# define __USE_LARGEFILE64	1
+#endif
+#endif
+
+#define __USE_GNU
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -83,3 +100,4 @@ int lockf64 (int fd, int cmd, off64_t len64)
 
     return fcntl(fd, cmd, &fl);
 }
+
