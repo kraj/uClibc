8b1cd1869a7a15a31153e579a1dde0b257d2f358
Eric Andersen <andersen@codepoet.org>
2003-02-26 02:13:56 +0000

    Prefer mmap2 for mips when available.  Fix the mmap test.  -Erik

--- libc/sysdeps/linux/mips/_mmap.c
+++ libc/sysdeps/linux/mips/_mmap.c
@@ -4,5 +4,10 @@
 #include <sys/mman.h>
 #include <sys/syscall.h>
 
+#ifdef __NR_mmap2
+# undef __NR_mmap
+# define __NR_mmap __NR_mmap2
+#endif
+
 _syscall6 (__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
 	   int, flags, int, fd, __off_t, offset);
--- test/mmap/mmap.c
+++ test/mmap/mmap.c
@@ -3,26 +3,25 @@
  * (as mmap) are done differently on various architectures.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <stdlib.h>
 
 
-int main(int argc,char *argv)
+int main(int argc, char **argv)
 {
 	void *ptr;
 
 
 	ptr = mmap(NULL, 4096, PROT_READ|PROT_WRITE,
-		MAP_PRIVATE|MAP_ANONYMOUS,
-		0, 0);
+		MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
 
 	if(ptr==MAP_FAILED){
 		perror("mmap");
 		exit(1);
-	}else{
-		printf("mmap returned %p\n",ptr);
-		exit(0);
 	}
+	printf("mmap returned %p\n",ptr);
+	exit(0);
 }
 
