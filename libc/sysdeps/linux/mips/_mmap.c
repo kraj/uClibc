c2b0e3d9b7f76c3e783e2e6cb3438398d8d23458
Eric Andersen <andersen@codepoet.org>
2003-03-06 16:56:02 +0000

    For now, leave mmap on mips using mmap1, since mmap2 seems to have 'issues' (i.e. it doesn't work 100% properly).  -Erik

--- libc/sysdeps/linux/mips/_mmap.c
+++ libc/sysdeps/linux/mips/_mmap.c
@@ -4,9 +4,14 @@
 #include <sys/mman.h>
 #include <sys/syscall.h>
 
+#if 0
+/* For now, leave mmap using mmap1 since mmap2 seems
+ * to have issues (i.e. it doesn't work 100% properly).
+ */
 #ifdef __NR_mmap2
 # undef __NR_mmap
 # define __NR_mmap __NR_mmap2
+#endif
 #endif
 
 _syscall6 (__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
