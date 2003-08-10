85a056b07157605c8bc15c873bb1a4116e6add60
Eric Andersen <andersen@codepoet.org>
2003-08-10 10:09:44 +0000

    silly me, thats not going to work.

--- include/features.h
+++ include/features.h
@@ -421,9 +421,8 @@ uClibc was built without large file support enabled.
 #if __GNUC__ == 2 && __GNUC_MINOR__ < 96
 #define __builtin_expect(x, expected_value) (x)
 #endif
-
-#define likely(x)		__builtin_expect(!((int)(x)),0)
-#define unlikely(x)		__builtin_expect(((int)(x)),0)
+#define likely(x)      __builtin_expect((!!(x)),1)
+#define unlikely(x)    __builtin_expect((x),0)
 
 /* --- this is added to integrate linuxthreads */
 #define __USE_UNIX98            1
