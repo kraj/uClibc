113471cb0bd16e03864a58983468ce8091fbecc3
Eric Andersen <andersen@codepoet.org>
2003-08-22 20:28:43 +0000

    Avoid gratuitous conflicts when used with kernel headers

--- include/features.h
+++ include/features.h
@@ -421,8 +421,12 @@ uClibc was built without large file support enabled.
 #if __GNUC__ == 2 && __GNUC_MINOR__ < 96
 #define __builtin_expect(x, expected_value) (x)
 #endif
-#define likely(x)	__builtin_expect((!!(x)),1)
-#define unlikely(x)	__builtin_expect((!!(x)),0)
+#ifndef likely
+# define likely(x)	__builtin_expect((!!(x)),1)
+#endif
+#ifndef unlikely
+# define unlikely(x)	__builtin_expect((!!(x)),0)
+#endif
 
 /* --- this is added to integrate linuxthreads */
 #define __USE_UNIX98            1
