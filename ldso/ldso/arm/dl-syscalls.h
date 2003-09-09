48c0f2f304fb0c73f292d7df5eaa463774dc6bf4
Eric Andersen <andersen@codepoet.org>
2003-09-09 06:11:11 +0000

    Arm needs this to compile with gcc 2.95

--- ldso/ldso/arm/dl-syscalls.h
+++ ldso/ldso/arm/dl-syscalls.h
@@ -3,5 +3,17 @@
  * before the errno symbol is dynamicly linked. */
 
 #define __set_errno(X) {(void)(X);}
+
+/* Prepare for the case that `__builtin_expect' is not available.  */
+#if __GNUC__ == 2 && __GNUC_MINOR__ < 96
+#define __builtin_expect(x, expected_value) (x)
+#endif
+#ifndef likely
+# define likely(x)	__builtin_expect((!!(x)),1)
+#endif
+#ifndef unlikely
+# define unlikely(x)	__builtin_expect((!!(x)),0)
+#endif
+
 #include "sys/syscall.h"
 
