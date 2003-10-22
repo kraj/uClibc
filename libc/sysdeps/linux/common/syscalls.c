8e8099dcac7317d161b4341dbac5369164397f82
Eric Andersen <andersen@codepoet.org>
2003-10-22 09:52:24 +0000

    Per suggestion and patch from Ken Staton, emulates poll using select for older 2.0 kernels where poll is missing.

--- libc/sysdeps/linux/common/syscalls.c
+++ libc/sysdeps/linux/common/syscalls.c
@@ -1721,9 +1721,12 @@ int getresuid (uid_t *ruid, uid_t *euid, uid_t *suid)
 #endif	
 
 //#define __NR_poll                     168
-#if defined(L_poll) && defined(__NR_poll) /* uClinux 2.0 doesn't have poll */
+#if defined(L_poll) && defined(__NR_poll)
 #include <sys/poll.h>
 _syscall3(int, poll, struct pollfd *, fds, unsigned long int, nfds, int, timeout);
+#else
+/* uClinux 2.0 doesn't have poll, emulate it using select */
+#include "poll.c"
 #endif
 
 //#define __NR_nfsservctl               169
