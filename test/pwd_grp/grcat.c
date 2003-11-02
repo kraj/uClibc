dae4f33e486b80e3d957e35355ca0a63cd90f4bd
Eric Andersen <andersen@codepoet.org>
2003-11-02 21:55:06 +0000

    Additional passwd and group tests

--- test/pwd_grp/grcat.c
+++ test/pwd_grp/grcat.c
@@ -8,40 +8,25 @@
  * Public Domain
  */
 
-/* For OS/2, do nothing. */
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#if defined (STDC_HEADERS)
 #include <stdlib.h>
-#endif
-
-#ifndef HAVE_GETGRENT
-int main() { return 0; }
-#else
 #include <stdio.h>
 #include <grp.h>
 
-int
-main(argc, argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
     struct group *g;
     int i;
 
     while ((g = getgrent()) != NULL) {
-        printf("%s:%s:%ld:", g->gr_name, g->gr_passwd,
-                                     (long) g->gr_gid);
-        for (i = 0; g->gr_mem[i] != NULL; i++) {
-            printf("%s", g->gr_mem[i]);
-            if (g->gr_mem[i+1] != NULL)
-                putchar(',');
-        }
-        putchar('\n');
+	printf("%s:%s:%ld:", g->gr_name, g->gr_passwd,
+		(long) g->gr_gid);
+	for (i = 0; g->gr_mem[i] != NULL; i++) {
+	    printf("%s", g->gr_mem[i]);
+	    if (g->gr_mem[i+1] != NULL)
+		putchar(',');
+	}
+	putchar('\n');
     }
     endgrent();
     return 0;
 }
-#endif /* HAVE_GETGRENT */
--- test/pwd_grp/pwcat.c
+++ test/pwd_grp/pwcat.c
@@ -8,28 +8,18 @@
  * Public Domain
  */
 
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <stdio.h>
 #include <pwd.h>
-
-#if defined (STDC_HEADERS)
 #include <stdlib.h>
-#endif
 
-int
-main(argc, argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
     struct passwd *p;
 
     while ((p = getpwent()) != NULL)
-        printf("%s:%s:%ld:%ld:%s:%s:%s\n",
-            p->pw_name, p->pw_passwd, (long) p->pw_uid,
-            (long) p->pw_gid, p->pw_gecos, p->pw_dir, p->pw_shell);
+	printf("%s:%s:%ld:%ld:%s:%s:%s\n",
+		p->pw_name, p->pw_passwd, (long) p->pw_uid,
+		(long) p->pw_gid, p->pw_gecos, p->pw_dir, p->pw_shell);
 
     endpwent();
     return 0;
