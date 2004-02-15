2148212370266d46c4bd2d370196bcce1c5fe7db
Eric Andersen <andersen@codepoet.org>
2004-02-15 09:34:42 +0000

    This should not be inlined.  This should probably use a weak alias as well...

--- libc/sysdeps/linux/e1/vfork.c
+++ libc/sysdeps/linux/e1/vfork.c
@@ -3,5 +3,5 @@
 #include <sys/errno.h>
 
 #define __NR___libc_vfork __NR_vfork
-inline	_syscall0(pid_t, __libc_vfork);
-inline	_syscall0(pid_t, vfork);
+_syscall0(pid_t, __libc_vfork);
+_syscall0(pid_t, vfork);
