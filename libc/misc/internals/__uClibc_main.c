fd4f96fda50d2462621de89762659e7cc7d648a9
Eric Andersen <andersen@codepoet.org>
2004-05-11 10:45:25 +0000

    Purge trailing white space

--- libc/misc/internals/__uClibc_main.c
+++ libc/misc/internals/__uClibc_main.c
@@ -40,7 +40,7 @@ extern void weak_function __pthread_initialize_minimal(void);
 
 /*
  * Declare the __environ global variable and create a weak alias environ.
- * Note: Apparently we must initialize __environ to ensure that the weak 
+ * Note: Apparently we must initialize __environ to ensure that the weak
  * environ symbol is also included.
  */
 
@@ -58,22 +58,22 @@ weak_alias(__environ, environ);
  * the libc code without restriction.  For this we link the shared
  * version of the uClibc with -init __uClibc_init so DT_INIT for
  * uClibc is the address of __uClibc_init
- * 
+ *
  * In all other cases we call it from the main stub
  * __uClibc_start_main.
  */
 
-void __uClibc_init(void) 
+void __uClibc_init(void)
 {
     static int been_there_done_that = 0;
 
-    if (been_there_done_that) 
+    if (been_there_done_that)
 	return;
     been_there_done_that++;
 
 #ifdef __UCLIBC_HAS_THREADS__
     /* Before we start initialzing uClibc we have to call
-     * __pthread_initialize_minimal so we can use pthread_locks 
+     * __pthread_initialize_minimal so we can use pthread_locks
      * whenever they are needed.
      */
     if (likely(__pthread_initialize_minimal!=NULL))
@@ -108,12 +108,12 @@ void __uClibc_init(void)
 void (*__app_fini)(void) = NULL;
 #endif
 
-/* __uClibc_start_main is the new main stub for uClibc. This function is 
- * called from crt0 (version 0.9.16 or newer), after ALL shared libraries 
+/* __uClibc_start_main is the new main stub for uClibc. This function is
+ * called from crt0 (version 0.9.16 or newer), after ALL shared libraries
  * are initialized, just before we call the application's main function.
  */
-void __attribute__ ((__noreturn__)) 
-__uClibc_start_main(int argc, char **argv, char **envp, 
+void __attribute__ ((__noreturn__))
+__uClibc_start_main(int argc, char **argv, char **envp,
 	void (*app_init)(void), void (*app_fini)(void))
 {
 
@@ -121,12 +121,12 @@ __uClibc_start_main(int argc, char **argv, char **envp,
      * did this for us.  But if we are statically linked, we need
      * to do this for ourselves.  */
     if (__environ==NULL) {
-	/* Statically linked. */ 
+	/* Statically linked. */
 	__environ = envp;
     }
 
     /* We need to initialize uClibc.  If we are dynamically linked this
-     * may have already been completed by the shared lib loader.  We call 
+     * may have already been completed by the shared lib loader.  We call
      * __uClibc_init() regardless, to be sure the right thing happens. */
     __uClibc_init();
 
@@ -167,14 +167,14 @@ __uClibc_start_main(int argc, char **argv, char **envp,
 /* __uClibc_main is the old main stub of the uClibc. This
  * function is called from crt0 (uClibc 0.9.15 and older) after
  * ALL shared libraries are initialized, and just before we call
- * the application's main() function.  
+ * the application's main() function.
  *
  * Attention: This stub does not call the .init/.fini sections of
  * the application. If you need this, please fix your uClibc port
  * so that  __uClibc_start_main is called by your crt0.S with
  * _init and _fini properly set.
 */
-void __attribute__ ((__noreturn__)) 
+void __attribute__ ((__noreturn__))
 __uClibc_main(int argc, char **argv, char ** envp)
 {
     __uClibc_start_main(argc, argv, envp, NULL, NULL);
