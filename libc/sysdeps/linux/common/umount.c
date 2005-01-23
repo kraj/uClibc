610c850501ff9d21d06b4a9942f2cfd2c347619e
Eric Andersen <andersen@codepoet.org>
2005-01-23 11:27:02 +0000

    When there is no umount syscall, but umount2 is available, then try to emulate umount() using umount2()

--- libc/sysdeps/linux/common/umount.c
+++ libc/sysdeps/linux/common/umount.c
@@ -12,6 +12,13 @@
 #ifdef __NR_umount	/* Some newer archs only have umount2 */
 #include <sys/mount.h>
 _syscall1(int, umount, const char *, specialfile);
+#elif defined __NR_umount2
+/* No umount syscall, but umount2 is available....  Try to
+ * emulate umount() using umount2() */
+int umount(const char *special_file, int flags)
+{
+	return _syscall2(int, umount2, const char *, special_file, int, flags);
+}
 #else
 int umount(const char *special_file)
 {
