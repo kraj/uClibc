fde774dc47375abbad649b5de91b8e96dcc014a8
Manuel Novoa III <mjn3@codepoet.org>
2005-03-17 06:41:59 +0000

    Revert last commit.  If I had wanted to include that patch, I would have done it myself at least a year and a half ago.

--- libc/stdio/_fpmaxtostr.c
+++ libc/stdio/_fpmaxtostr.c
@@ -205,7 +205,7 @@ size_t _fpmaxtostr(FILE * fp, __fpmax_t x, struct printf_info *info,
 	__fpmax_t lower_bnd;
 	__fpmax_t upper_bnd = 1e9;
 #endif /* __UCLIBC_HAS_HEXADECIMAL_FLOATS__ */
-	volatile uint_fast32_t digit_block;
+	uint_fast32_t digit_block;
 #ifdef __UCLIBC_HAS_HEXADECIMAL_FLOATS__
 	uint_fast32_t base = 10;
 	const __fpmax_t *power_table;
@@ -221,9 +221,9 @@ size_t _fpmaxtostr(FILE * fp, __fpmax_t x, struct printf_info *info,
 	int nblk2;			   /* This does not need to be initialized. */
 	const char *ts;		   /* This does not need to be initialized. */
 #endif /* __UCLIBC_HAS_GLIBC_DIGIT_GROUPING__ */
-	volatile int i, j;
+	int i, j;
 	int round, o_exp;
-	volatile int exp, exp_neg;
+	int exp, exp_neg;
 	int width, preci;
 	int cnt;
 	char *s;
