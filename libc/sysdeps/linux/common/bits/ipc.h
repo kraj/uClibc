1f29aa48288c3e8ac9601fbe21b043772ca3eae1
Mike Frysinger <vapier@gentoo.org>
2005-06-17 02:20:24 +0000

    sync whitespace with glibc

--- libc/sysdeps/linux/common/bits/ipc.h
+++ libc/sysdeps/linux/common/bits/ipc.h
@@ -38,19 +38,19 @@
 /* Special key values.  */
 #define IPC_PRIVATE	((__key_t) 0)	/* Private key.  */
 
+
 /* Data structure used to pass permission information to IPC operations.  */
 struct ipc_perm
   {
-    __key_t __key;		/* Key.  */
-    __uid_t uid;		/* Owner's user ID.  */
-    __gid_t gid;		/* Owner's group ID.  */
-    __uid_t cuid;		/* Creator's user ID.  */
-    __gid_t cgid;		/* Creator's group ID.  */
-    unsigned short int mode;	/* Read/write permission.  */
+    __key_t __key;			/* Key.  */
+    __uid_t uid;			/* Owner's user ID.  */
+    __gid_t gid;			/* Owner's group ID.  */
+    __uid_t cuid;			/* Creator's user ID.  */
+    __gid_t cgid;			/* Creator's group ID.  */
+    unsigned short int mode;		/* Read/write permission.  */
     unsigned short int __pad1;
-    unsigned short int __seq;	/* Sequence number.  */
+    unsigned short int __seq;		/* Sequence number.  */
     unsigned short int __pad2;
     unsigned long int __unused1;
     unsigned long int __unused2;
   };
-
