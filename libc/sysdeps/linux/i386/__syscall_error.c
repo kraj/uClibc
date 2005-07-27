2f6f7d535ce6321444e961c1ef536f02948f58fd
Mike Frysinger <vapier@gentoo.org>
2005-07-27 02:02:09 +0000

    add some asm magic to fix setting errno/ret values in the .S functions

--- libc/sysdeps/linux/i386/__syscall_error.c
+++ libc/sysdeps/linux/i386/__syscall_error.c
@@ -22,8 +22,24 @@
 
 /* This routine is jumped to by all the syscall handlers, to stash
  * an error number into errno.  */
-int attribute_hidden __syscall_error(int err_no)
+
+/* This version uses a lot of magic and relies heavily on x86 
+ * calling convention ... The advantage is that this is the same 
+ * size as the previous __syscall_error() but all the .S functions
+ * need just one instruction.
+ *
+ * Local .S files have to set %eax to the negative errno value 
+ * and then jump to this function.  The neglected return to caller 
+ * and return value of -1 is taken care of here so we don't have to 
+ * worry about it in the .S functions.
+ *
+ * We have to stash the errno from %eax in a local stack var because 
+ * __set_errno will prob call a function thus clobbering %eax on us.
+ */
+void attribute_hidden __syscall_error(void)
 {
-	__set_errno(err_no);
-	return -1;
+	register int eax asm("%eax");
+	int stack = -eax;
+	__set_errno(stack);
+	eax = -1;
 }
