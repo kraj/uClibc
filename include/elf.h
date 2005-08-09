bbde871b62cce4ef4acc1659845f2d49de3d8105
Mike Frysinger <vapier@gentoo.org>
2005-08-09 22:20:06 +0000

    date: 2005/08/07 07:53:58;  author: drepper;  state: Exp;  lines: +1 -1 (DT_ALPHA_PLTRO): Use symbolic name in DT_ALPHA_PLTRO definition.

--- include/elf.h
+++ include/elf.h
@@ -1910,7 +1910,7 @@ typedef Elf32_Addr Elf32_Conflict;
 #define LITUSE_ALPHA_TLS_LDM	5
 
 /* Legal values for d_tag of Elf64_Dyn.  */
-#define DT_ALPHA_PLTRO		0x70000000
+#define DT_ALPHA_PLTRO		(DT_LOPROC + 0)
 #define DT_ALPHA_NUM		1
 
 /* PowerPC specific declarations */
