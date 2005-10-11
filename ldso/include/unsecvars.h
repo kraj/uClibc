37eed2db12e37a6d9592a688ab51843063b37ecc
Peter S. Mazinger <ps.m@gmx.net>
2005-10-11 08:58:53 +0000

    Sync unsecvars.h w/ glibc-2.3.5, remove unused vars in uClibc, update comment about those in glibc

--- ldso/include/unsecvars.h
+++ ldso/include/unsecvars.h
@@ -5,22 +5,21 @@
  */
 
 #define UNSECURE_ENVVARS \
-	"LD_AOUT_PRELOAD\0" \
-	"LD_AOUT_LIBRARY_PATH\0" \
 	"LD_PRELOAD\0" \
 	"LD_LIBRARY_PATH\0" \
 	"LD_DEBUG\0" \
 	"LD_DEBUG_OUTPUT\0" \
 	"LD_TRACE_LOADED_OBJECTS\0" \
-	"HOSTALIASES\0" \
-	"LOCALDOMAIN\0" \
-	"RES_OPTIONS\0" \
 	"TMPDIR\0"
 
 /* 
+ * LD_TRACE_LOADED_OBJECTS is not in glibc-2.3.5's unsecvars.h
+ * though used by ldd
+ *
  * These environment variables are defined by glibc but ignored in
  * uClibc, but may very well have an equivalent in uClibc.
  *
- * MALLOC_TRACE, RESOLV_HOST_CONF, TZDIR, GCONV_PATH, LD_USE_LOAD_BIAS,
- * LD_PROFILE, LD_ORIGIN_PATH, LOCPATH, NLSPATH
+ * LD_ORIGIN_PATH, LD_PROFILE, LD_USE_LOAD_BIAS, LD_DYNAMIC_WEAK, LD_SHOW_AUXV,
+ * GCONV_PATH, GETCONF_DIR, HOSTALIASES, LOCALDOMAIN, LOCPATH, MALLOC_TRACE,
+ * NLSPATH, RESOLV_HOST_CONF, RES_OPTIONS, TMPDIR, TZDIR
  */
