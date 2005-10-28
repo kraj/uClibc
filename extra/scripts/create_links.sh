358043ffb9380926813eb8200f0a45140acdb77a
Peter S. Mazinger <ps.m@gmx.net>
2005-10-28 09:57:22 +0000

    Renamed arch specific Makefile.in to Makefile.arch, else if TARGET_ARCH does not exist, we reinclude the including Makefile. Moved arch/common fpu_control.h link creation into main Makefile.in. Updated the link creation script to remove all the other Makefiles

--- extra/scripts/create_links.sh
+++ extra/scripts/create_links.sh
@@ -7,18 +7,19 @@ DIRS2="ldso/ldso ldso/libdl libpthread/linuxthreads libpthread/linuxthreads_db"
 OBJ4="common arm i386 mips powerpc x86_64"
 
 for x in ${DIRS} ; do
-	rm -f ${x}/Makefile
+	find ${x} -type f -name Makefile -exec rm -f {} \;
 	ln -sf ../extra/scripts/Makefile.libs.lvl1 ${x}/Makefile
 done
 
 for x in ${DIRS2} ; do
-	rm -f ${x}/Makefile
 	ln -sf ../../extra/scripts/Makefile.libs.lvl2 ${x}/Makefile
 done
 
 for x in ${OBJ4} ; do
-	rm -f libc/sysdeps/linux/${x}/Makefile
 	ln -sf ../../../../extra/scripts/Makefile.objs.lvl4 libc/sysdeps/linux/${x}/Makefile
 done
+
+rm -f Makefile
+ln -sf Makefile.in Makefile
 
 exit 0
