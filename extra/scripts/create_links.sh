2386d6cd13271699bc9e97412e1ca4273e247c47
Peter S. Mazinger <ps.m@gmx.net>
2005-10-28 10:55:11 +0000

    Use consequently headers-y and headers_clean-y, try avoiding TARGET_ARCH use in main Makefile.in

--- extra/scripts/create_links.sh
+++ extra/scripts/create_links.sh
@@ -4,7 +4,6 @@
 
 DIRS="ldso libc libcrypt libintl libm libnsl libpthread libresolv librt libutil"
 DIRS2="ldso/ldso ldso/libdl libpthread/linuxthreads libpthread/linuxthreads_db"
-OBJ4="common arm i386 mips powerpc x86_64"
 
 for x in ${DIRS} ; do
 	find ${x} -type f -name Makefile -exec rm -f {} \;
@@ -15,9 +14,8 @@ for x in ${DIRS2} ; do
 	ln -sf ../../extra/scripts/Makefile.libs.lvl2 ${x}/Makefile
 done
 
-for x in ${OBJ4} ; do
-	ln -sf ../../../../extra/scripts/Makefile.objs.lvl4 libc/sysdeps/linux/${x}/Makefile
-done
+ln -sf ../../../extra/scripts/Makefile.objs.lvl3 libc/sysdeps/linux/Makefile
+ln -sf ../../extra/scripts/Makefile.objs.lvl2 libc/string/Makefile
 
 rm -f Makefile
 ln -sf Makefile.in Makefile
