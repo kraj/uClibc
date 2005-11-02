e21214c4258a3a20ce026ac0369270c398dbb71e
Peter S. Mazinger <ps.m@gmx.net>
2005-11-02 21:48:56 +0000

    Mark _exit as noreturn, include/unistd.h does this already

--- libc/sysdeps/linux/common/_exit.c
+++ libc/sysdeps/linux/common/_exit.c
@@ -32,7 +32,7 @@
 static inline _syscall1(void, __syscall_exit, int, status);
 #endif
 
-void _exit(int status)
+void __attribute__ ((noreturn)) _exit(int status)
 {
 	/* The loop is added only to keep gcc happy. */
 	while(1)
