063957fd156b241904a447ef1d4d9ec5e674e6f1
Peter S. Mazinger <ps.m@gmx.net>
2005-11-03 19:16:21 +0000

    wait4 is pid_t, not int

--- libc/sysdeps/linux/common/wait4.c
+++ libc/sysdeps/linux/common/wait4.c
@@ -12,9 +12,9 @@
 
 #define __NR___syscall_wait4 __NR_wait4
 static inline _syscall4(int, __syscall_wait4, __kernel_pid_t, pid,
-		int *, status, int, opts, void *, rusage);
+		int *, status, int, opts, struct rusage *, rusage);
 
-int wait4(pid_t pid, int *status, int opts, struct rusage *rusage)
+pid_t wait4(pid_t pid, int *status, int opts, struct rusage *rusage)
 {
 	return (__syscall_wait4(pid, status, opts, rusage));
 }
