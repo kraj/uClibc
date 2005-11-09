b8fed78ba4359398198708d715545e8f991e5d60
Mike Frysinger <vapier@gentoo.org>
2005-11-09 03:06:01 +0000

    booooring license updates

--- libc/sysdeps/linux/common/bits/uClibc_va_copy.h
+++ libc/sysdeps/linux/common/bits/uClibc_va_copy.h
@@ -1,8 +1,21 @@
 /* Copyright (C) 2005       Manuel Novoa III    <mjn3@codepoet.org>
  *
- * GNU Library General Public License (LGPL) version 2 or later.
- *
  * Dedicated to Toni.  See uClibc/DEDICATION.mjn3 for details.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  The GNU C Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with the GNU C Library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA.
  */
 
 #ifndef _UCLIBC_VA_COPY_H
