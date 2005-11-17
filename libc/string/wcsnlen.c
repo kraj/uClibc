c26874ea72f6e339759a58b195ac75dd72d56f20
"Steven J. Hill" <sjhill@realitydiluted.com>
2005-11-17 02:23:09 +0000

    Copy from trunk.

---
+++ libc/string/wcsnlen.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2000-2005 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+
+#define L_strnlen
+#define WANT_WIDE
+#define Wstrnlen __wcsnlen
+
+#include "wstring.c"
+
+strong_alias(__wcsnlen, wcsnlen)
+
+#undef L_strnlen
