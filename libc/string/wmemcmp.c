67f62274d2b2c1406ef8bd444a219043e22afabf
"Steven J. Hill" <sjhill@realitydiluted.com>
2005-11-17 02:24:22 +0000

    Copy from trunk.

---
+++ libc/string/wmemcmp.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2000-2005 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+
+#define L_memcmp
+#define WANT_WIDE
+#define Wmemcmp __wmemcmp
+
+#include "wstring.c"
+
+strong_alias(__wmemcmp, wmemcmp)
+
+#undef L_memcmp
