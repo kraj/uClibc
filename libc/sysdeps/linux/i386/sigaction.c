2cb0d0f00820ba04b552383e752e1d11b1f8c56b
Mike Frysinger <vapier@gentoo.org>
2005-11-21 23:57:49 +0000

    tweak restore function definitions like glibc does it to get rid of warnings

--- libc/sysdeps/linux/i386/sigaction.c
+++ libc/sysdeps/linux/i386/sigaction.c
@@ -29,9 +29,8 @@
 
 
 #if defined __NR_rt_sigaction
-#warning "Yes there are two warnings here.  Don't worry about it."
-static void restore_rt (void) asm ("__restore_rt");
-static void restore (void) asm ("__restore");
+extern void restore_rt (void) asm ("__restore_rt") attribute_hidden;
+extern void restore (void) asm ("__restore") attribute_hidden;
 
 /* If ACT is not NULL, change the action for SIG to *ACT.
    If OACT is not NULL, put the old action for SIG in *OACT.  */
@@ -73,8 +72,7 @@ int __libc_sigaction (int sig, const struct sigaction *act, struct sigaction *oa
 
 
 #else
-#warning "Yes there is a warning here.  Don't worry about it."
-static void restore (void) asm ("__restore");
+extern void restore (void) asm ("__restore") attribute_hidden;
 
 /* If ACT is not NULL, change the action for SIG to *ACT.
    If OACT is not NULL, put the old action for SIG in *OACT.  */
