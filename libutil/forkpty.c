26340613a0ee686d269e142ed75367a916e10311
Mike Frysinger <vapier@gentoo.org>
2005-11-23 12:53:58 +0000

    sync with glibc

--- libutil/forkpty.c
+++ libutil/forkpty.c
@@ -3,19 +3,22 @@
    Contributed by Zack Weinberg <zack@rabi.phys.columbia.edu>, 1998.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#define openpty __openpty
+#define login_tty __login_tty
 
 #include <sys/types.h>
 #include <termios.h>
@@ -23,34 +26,34 @@
 #include <utmp.h>
 #include <pty.h>
 
-extern int __openpty (int *amaster, int *aslave, char *name, struct termios *termp,
-						struct winsize *winp) attribute_hidden;
-extern int __login_tty(int fd) attribute_hidden;
-
-int forkpty (int *amaster, char *name, 
-	struct termios *termp, struct winsize *winp)
+int
+forkpty (amaster, name, termp, winp)
+     int *amaster;
+     char *name;
+     struct termios *termp;
+     struct winsize *winp;
 {
-    int master, slave, pid;
+  int master, slave, pid;
 
-    if (__openpty (&master, &slave, name, termp, winp) == -1)
-	return -1;
+  if (openpty (&master, &slave, name, termp, winp) == -1)
+    return -1;
 
-    switch (pid = fork ())
+  switch (pid = fork ())
     {
-	case -1:
-	    return -1;
-	case 0:
-	    /* Child.  */
-	    close (master);
-	    if (__login_tty (slave))
-		_exit (1);
+    case -1:
+      return -1;
+    case 0:
+      /* Child.  */
+      close (master);
+      if (login_tty (slave))
+	_exit (1);
 
-	    return 0;
-	default:
-	    /* Parent.  */
-	    *amaster = master;
-	    close (slave);
+      return 0;
+    default:
+      /* Parent.  */
+      *amaster = master;
+      close (slave);
 
-	    return pid;
+      return pid;
     }
 }
--- libutil/openpty.c
+++ libutil/openpty.c
@@ -1,21 +1,21 @@
-/* Copyright (C) 1998, 1999 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 1999, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Zack Weinberg <zack@rabi.phys.columbia.edu>, 1998.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 #include <errno.h>
 #include <fcntl.h>
@@ -72,7 +72,7 @@ pts_name (int fd, char **pts, size_t buf_len)
       if (! new_buf)
 	{
 	  rv = -1;
-	  errno = ENOMEM;
+	  __set_errno (ENOMEM);
 	  break;
 	}
       buf = new_buf;
