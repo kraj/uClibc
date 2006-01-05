537d67f25d665032b9b86693f283203be11a2f97
Mike Frysinger <vapier@gentoo.org>
2006-01-05 23:47:53 +0000

    grab syscall macros from linux headers

--- libc/sysdeps/linux/bfin/bits/syscalls.h
+++ libc/sysdeps/linux/bfin/bits/syscalls.h
@@ -6,14 +6,138 @@
 
 #include <features.h>
 
-/* Do something very evil for now.  Until we create our own syscall
- * macros, short circuit bits/sysnum.h  and use asm/unistd.h instead */
-#include <asm/unistd.h>
-
 /* This includes the `__NR_<name>' syscall numbers taken from the Linux kernel
  * header files.  It also defines the traditional `SYS_<name>' macros for older
  * programs.  */
 #include <bits/sysnum.h>
 
-#endif /* _BITS_SYSCALLS_H */
+/* This code is mostly cut & paste from the uClinux bfin port */
 
+#ifndef __ASSEMBLER__
+
+#define __syscall_return(type, res)					\
+do {									\
+	if ((unsigned long)(res) >= (unsigned long)(-125)) 		\
+	{	__set_errno(-(res));					\
+		res = -1;						\
+	}								\
+	return (type) (res);						\
+} while (0)
+
+#define _syscall0(type,name)						\
+type name(void) {							\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "p0 = %1;\n\t"							\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+  : "=da" (__res) 							\
+  : "i" (__NR_##name)							\
+  : "CC", "P0");							\
+__syscall_return(type,__res);						\
+}
+
+#define _syscall1(type,name,type1,arg1)					\
+type name(type1 arg1) {							\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "r0=%2;\n\t"								\
+  "p0=%1;\n\t"								\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+        : "=da" (__res)							\
+        : "i" (__NR_##name),						\
+	  "a" ((long)(arg1))						\
+	: "CC", "R0", "P0");						\
+__syscall_return(type,__res);						\
+}
+
+#define _syscall2(type,name,type1,arg1,type2,arg2)			\
+type name(type1 arg1,type2 arg2) {					\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "r1=%3;\n\t"								\
+  "r0=%2;\n\t"								\
+  "p0=%1;\n\t"								\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+        : "=da" (__res)							\
+        : "i" (__NR_##name),						\
+	  "a" ((long)(arg1)),						\
+	  "a" ((long)(arg2))						\
+	: "CC", "R0","R1", "P0");					\
+__syscall_return(type,__res);						\
+}
+
+
+#define _syscall3(type,name,type1,arg1,type2,arg2,type3,arg3)		\
+type name(type1 arg1,type2 arg2,type3 arg3) {				\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "r2=%4;\n\t"								\
+  "r1=%3;\n\t"								\
+  "r0=%2;\n\t"								\
+  "p0=%1;\n\t"								\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+        : "=da" (__res)							\
+        : "i"   (__NR_##name),						\
+	  "a"   ((long)(arg1)),						\
+	  "a"   ((long)(arg2)),						\
+	  "a"   ((long)(arg3))						\
+        : "CC", "R0","R1","R2", "P0");					\
+__syscall_return(type,__res);						\
+}
+
+#define _syscall4(type,name,type1,arg1,type2,arg2,type3,arg3,type4,arg4)\
+type name(type1 arg1, type2 arg2, type3 arg3, type4 arg4) {		\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "[--sp] = r3;\n\t"							\
+  "r3=%5;\n\t"								\
+  "r2=%4;\n\t"								\
+  "r1=%3;\n\t"								\
+  "r0=%2;\n\t"								\
+  "p0=%1;\n\t"								\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+  "r3 = [sp++];\n\t"							\
+  	: "=da" (__res)							\
+  	: "i"  (__NR_##name),						\
+	  "a"  ((long)(arg1)),						\
+	  "a"  ((long)(arg2)),						\
+	  "a"  ((long)(arg3)),						\
+	  "a"  ((long)(arg4))						\
+  	: "CC", "R0","R1","R2","R3", "P0");				\
+__syscall_return(type,__res);						\
+}
+
+#define _syscall5(type,name,type1,arg1,type2,arg2,type3,arg3,type4,arg4,type5,arg5)	\
+type name(type1 arg1, type2 arg2, type3 arg3, type4 arg4, type5 arg5) {	\
+  long __res;								\
+  __asm__ __volatile__ (						\
+  "[--sp] = r4;\n\t"                                                    \
+  "[--sp] = r3;\n\t"                                                    \
+  "r4=%6;\n\t"								\
+  "r3=%5;\n\t"								\
+  "r2=%4;\n\t"								\
+  "r1=%3;\n\t"								\
+  "r0=%2;\n\t"								\
+  "P0=%1;\n\t"								\
+  "excpt 0;\n\t" 							\
+  "%0=r0;\n\t"								\
+  "r3 = [sp++];\n\t" 							\
+  "r4 = [sp++];\n\t"                                                    \
+  	: "=da" (__res)							\
+  	: "i"  (__NR_##name),						\
+	  "rm"  ((long)(arg1)),						\
+	  "rm"  ((long)(arg2)),						\
+	  "rm"  ((long)(arg3)),						\
+	  "rm"  ((long)(arg4)),						\
+	  "rm"  ((long)(arg5))						\
+	: "CC","R0","R1","R2","R3","R4","P0");				\
+__syscall_return(type,__res);						\
+}
+
+#endif /* __ASSEMBLER__ */
+#endif /* _BITS_SYSCALLS_H */
