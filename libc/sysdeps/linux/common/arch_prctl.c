cd7736ffa0dd5018fda249d95835cf17ca159094
Mike Frysinger <vapier@gentoo.org>
2006-01-23 22:46:26 +0000

    add support for arch_prctl() (only used on x86_64 atm)

---
+++ libc/sysdeps/linux/common/arch_prctl.c
@@ -0,0 +1,14 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * arch_prctl() for uClibc
+ *
+ * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+
+#include "syscalls.h"
+
+#ifdef __NR_arch_prctl
+_syscall2(int, arch_prctl, int, cod, unsigned long, addr);
+#endif
