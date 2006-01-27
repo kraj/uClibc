23eaf72eb88953da4452c30d4974113b9527952c
Peter S. Mazinger <ps.m@gmx.net>
2006-01-27 14:57:54 +0000

    s|sys/perm.h|sys/io.h|, thx vapier

--- libc/sysdeps/linux/common/iopl.c
+++ libc/sysdeps/linux/common/iopl.c
@@ -10,6 +10,6 @@
 #include "syscalls.h"
 /* Tuns out the m68k unistd.h kernel header is broken */
 #if defined __ARCH_HAS_MMU__ && defined __NR_iopl && ( !defined(__mc68000__))
-#include <sys/perm.h>
+#include <sys/io.h>
 _syscall1(int, iopl, int, level);
 #endif
