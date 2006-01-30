fd9ddddb9009221c3e374d820b9854109ae825f6
Peter S. Mazinger <ps.m@gmx.net>
2006-01-30 09:39:51 +0000

    make it build if WCHAR is disabled

--- libc/sysdeps/linux/arm/aeabi_mb_cur_max.c
+++ libc/sysdeps/linux/arm/aeabi_mb_cur_max.c
@@ -20,10 +20,16 @@
 #include <locale.h>
 #include <stdlib.h>
 
+#ifdef __UCLIBC_HAS_WCHAR__
 libc_hidden_proto(_stdlib_mb_cur_max)
+#endif
 
 int
 __aeabi_MB_CUR_MAX (void)
 {
+#ifdef __UCLIBC_HAS_WCHAR__
   return MB_CUR_MAX;
+#else
+  return 1;
+#endif
 }
