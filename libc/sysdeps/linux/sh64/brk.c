1de7b5deae65d052672e409d5d4a99e05858896a
Peter S. Mazinger <ps.m@gmx.net>
2006-02-01 11:58:12 +0000

    missed two __curbrk

--- libc/sysdeps/linux/sh64/brk.c
+++ libc/sysdeps/linux/sh64/brk.c
@@ -12,8 +12,10 @@
 libc_hidden_proto(brk)
 
 extern void * __curbrk;
+libc_hidden_proto(__curbrk)
 
 extern int __init_brk (void);
+libc_hidden_proto(__init_brk)
 extern void *_brk(void *ptr) attribute_hidden;
 
 int brk(void * end_data_seg)
