c0eafa7e95363d6539e60398b1c5bf5555981f7e
Mike Frysinger <vapier@gentoo.org>
2006-02-22 03:34:51 +0000

    simplify epoll code

--- libc/sysdeps/linux/common/epoll.c
+++ libc/sysdeps/linux/common/epoll.c
@@ -14,52 +14,37 @@
  * epoll_create()
  */
 #ifdef __NR_epoll_create
-#define __NR___syscall_epoll_create __NR_epoll_create
-static inline _syscall1(int, __syscall_epoll_create, int, size);
-#endif
-
+_syscall1(int, epoll_create, int, size);
+#else
 int epoll_create(int size)
 {
-#ifdef __NR_epoll_create
-	return (__syscall_epoll_create(size));
-#else
     __set_errno(ENOSYS);
     return -1;
-#endif
 }
+#endif
 
 /*
  * epoll_ctl()
  */
 #ifdef __NR_epoll_ctl
-#define __NR___syscall_epoll_ctl __NR_epoll_ctl
-static inline _syscall4(int, __syscall_epoll_ctl, int, epfd, int, op, int, fd, struct epoll_event *, event);
-#endif
-
+_syscall4(int,epoll_ctl, int, epfd, int, op, int, fd, struct epoll_event *, event);
+#else
 int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
 {
-#ifdef __NR_epoll_ctl
-	return (__syscall_epoll_ctl(epfd, op, fd, event));
-#else
 	__set_errno(ENOSYS);
 	return -1;
-#endif
 }
+#endif
 
 /*
  * epoll_wait()
  */
 #ifdef __NR_epoll_wait
-#define __NR___syscall_epoll_wait __NR_epoll_wait
-static inline _syscall4(int, __syscall_epoll_wait, int, epfd, struct epoll_event *, events, int, maxevents, int, timeout);
-#endif
-
+_syscall4(int, epoll_wait, int, epfd, struct epoll_event *, events, int, maxevents, int, timeout);
+#else
 int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 {
-#ifdef __NR_epoll_wait
-	return (__syscall_epoll_wait(epfd, events, maxevents, timeout));
-#else
 	__set_errno(ENOSYS);
 	return -1;
-#endif
 }
+#endif
