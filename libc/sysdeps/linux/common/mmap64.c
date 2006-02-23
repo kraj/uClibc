519e5c203b0c30f37f198858bc8989dc69e46809
Peter Kjellerstedt <peter.kjellerstedt@axis.com>
2006-02-23 15:54:13 +0000

    A little clean up.

--- libc/sysdeps/linux/common/mmap64.c
+++ libc/sysdeps/linux/common/mmap64.c
@@ -14,7 +14,7 @@
 
 libc_hidden_proto(mmap)
 
-#if defined _FILE_OFFSET_BITS && _FILE_OFFSET_BITS != 64 
+#if defined _FILE_OFFSET_BITS && _FILE_OFFSET_BITS != 64
 # undef _FILE_OFFSET_BITS
 # define _FILE_OFFSET_BITS   64
 #endif
@@ -22,36 +22,38 @@ libc_hidden_proto(mmap)
 # define __USE_LARGEFILE64      1
 #endif
 /* We absolutely do _NOT_ want interfaces silently
- *  * renamed under us or very bad things will happen... */
+ * renamed under us or very bad things will happen...
+ */
 #ifdef __USE_FILE_OFFSET64
 # undef __USE_FILE_OFFSET64
 #endif
 
-# if ! defined __NR_mmap2 || ! defined _syscall6
+# if !defined __NR_mmap2 || !defined _syscall6
 
-/* 
+/*
  * This version is a stub that just chops off everything at the mmap 32 bit
  * mmap() address space...  You will probably need to add in an arch specific
  * implementation to override this as there is not a generic way for me to
  * implement this particular syscall if your arch lacks _syscall6...
  *
-*/
+ */
 
 __ptr_t mmap64(__ptr_t addr, size_t len, int prot, int flags, int fd, __off64_t offset)
 {
-	if (offset != (off_t) offset || (offset + len) != (off_t) (offset + len)) {
-		__set_errno (EINVAL);
+	if (offset != (off_t) offset ||
+	    (offset + len) != (off_t) (offset + len)) {
+		__set_errno(EINVAL);
 		return MAP_FAILED;
 	}
 
-	return mmap (addr, len, prot, flags, fd, (off_t) offset);
+	return mmap(addr, len, prot, flags, fd, (off_t) offset);
 }
 
 # else
 
 #  define __NR___syscall_mmap2	    __NR_mmap2
-static inline _syscall6(__ptr_t, __syscall_mmap2, __ptr_t, addr, 
-	size_t, len, int, prot, int, flags, int, fd, off_t, offset);
+static inline _syscall6(__ptr_t, __syscall_mmap2, __ptr_t, addr, size_t, len,
+                        int, prot, int, flags, int, fd, off_t, offset);
 
 /* Some architectures always use 12 as page shift for mmap2() eventhough the
  * real PAGE_SHIFT != 12.  Other architectures use the same value as
@@ -64,10 +66,11 @@ static inline _syscall6(__ptr_t, __syscall_mmap2, __ptr_t, addr,
 __ptr_t mmap64(__ptr_t addr, size_t len, int prot, int flags, int fd, __off64_t offset)
 {
 	if (offset & ((1 << MMAP2_PAGE_SHIFT) - 1)) {
-		__set_errno (EINVAL);
+		__set_errno(EINVAL);
 		return MAP_FAILED;
 	}
-	return(__syscall_mmap2(addr, len, prot, flags, fd, (off_t) (offset >> MMAP2_PAGE_SHIFT)));
+
+	return __syscall_mmap2(addr, len, prot, flags, fd, (off_t) (offset >> MMAP2_PAGE_SHIFT));
 }
 
 # endif
