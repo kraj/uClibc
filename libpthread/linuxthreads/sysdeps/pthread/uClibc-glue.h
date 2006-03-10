a219c7ddc96c93e2abe2aeebc064253f94d58000
Peter S. Mazinger <ps.m@gmx.net>
2006-03-10 00:38:24 +0000

    try to get libpthread built w/ -DSHARED, what to do w/ _dl_load_lock?

--- libpthread/linuxthreads/sysdeps/pthread/uClibc-glue.h
+++ libpthread/linuxthreads/sysdeps/pthread/uClibc-glue.h
@@ -29,6 +29,12 @@
 #define __libc_current_sigrtmin_private __libc_current_sigrtmin
 #define __clone clone
 
+/* taken from ldsodefs.h, modified */
+#define GL(name) _##name
+void (*_dl_rtld_lock_recursive) (void *);
+void (*_dl_rtld_unlock_recursive) (void *);
+void **(*_dl_error_catch_tsd) (void) __attribute__ ((const));
+
 extern void *__libc_stack_end;
 extern int __cxa_atexit (void (*func) (void *), void *arg, void *d);
 
