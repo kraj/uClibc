20dd29a97bf1cb69197c2a87173930d143ec8b7d
Mike Frysinger <vapier@gentoo.org>
2006-05-26 01:34:08 +0000

    the 16/32 swap funcs were swapped #875

--- utils/bswap.h
+++ utils/bswap.h
@@ -33,12 +33,12 @@
 #include <byteswap.h>
 #else
 
-static inline uint32_t bswap_32(uint32_t x)
+static inline uint16_t bswap_16(uint16_t x)
 {
 	return ((((x) & 0xff00) >> 8) | \
 	        (((x) & 0x00ff) << 8));
 }
-static inline uint16_t bswap_16(uint16_t x)
+static inline uint32_t bswap_32(uint32_t x)
 {
 	return ((((x) & 0xff000000) >> 24) | \
 	        (((x) & 0x00ff0000) >>  8) | \
