c83ff9adbaa0b4761f4d8f99778104f825e5a4a3
Mike Frysinger <vapier@gentoo.org>
2006-06-26 23:52:09 +0000

    use local labels

--- libc/sysdeps/linux/bfin/clone.c
+++ libc/sysdeps/linux/bfin/clone.c
@@ -24,7 +24,7 @@ clone (int (*fn)(void *arg), void *child_stack, int flags, void *arg, ...)
 			"excpt 0;"	 /*Call sys_clone*/
 			"%0  = r0;"
 			"cc = r0 == 0;"
-			"if !cc jump xxx;"	/* if (rval != 0) skip to parent */
+			"if !cc jump .Lxxx;"	/* if (rval != 0) skip to parent */
 			"r0 = %4;"
 			"p0 = %5;"
 			"fp = 0;"
@@ -33,7 +33,7 @@ clone (int (*fn)(void *arg), void *child_stack, int flags, void *arg, ...)
 			"call (p1);"	/* Call cloned function */
 			"p0 = %6;"
 			"excpt 0;"	/* Call sys_exit */
-			"xxx: nop;"
+			".Lxxx: nop;"
 			: "=d" (rval)
 			: "i" (__NR_clone), "a" (child_stack), "a" (flags), "a" (arg), "a" (fn), "i" (__NR_exit)
 			: "CC", "R0", "R1", "P0");
@@ -45,14 +45,14 @@ clone (int (*fn)(void *arg), void *child_stack, int flags, void *arg, ...)
 			"excpt 0;"	 /*Call sys_clone*/
 			"%0  = r0;"
 			"cc = r0 == 0;"
-			"if !cc jump xxx;"	/* if (rval != 0) skip to parent */
+			"if !cc jump .Lxxx;"	/* if (rval != 0) skip to parent */
 			"r0 = %4;"
 			"p0 = %5;"
 			"fp = 0;"
 			"call (p0);"	/* Call cloned function */
 			"p0 = %6;"
 			"excpt 0;"	/* Call sys_exit */
-			"xxx: nop;"
+			".Lxxx: nop;"
 			: "=d" (rval)
 			: "i" (__NR_clone), "a" (child_stack), "a" (flags), "a" (arg), "a" (fn), "i" (__NR_exit)
 			: "CC", "R0", "R1", "P0");
