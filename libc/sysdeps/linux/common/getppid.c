08dec285fab31afa558df7bf8a0c53e7db7910e2
Mike Frysinger <vapier@gentoo.org>
2006-07-05 10:36:32 +0000

    update license

--- libc/sysdeps/linux/common/getppid.c
+++ libc/sysdeps/linux/common/getppid.c
@@ -4,7 +4,7 @@
  *
  * Copyright (C) 2000-2006 by Erik Andersen <andersen@codepoet.org>
  *
- * GNU Library General Public License (LGPL) version 2 or later.
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
 #include "syscalls.h"
