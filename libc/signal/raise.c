f62a6df27c24796059a9d105342778b53835f1d1
Mike Frysinger <vapier@gentoo.org>
2006-07-05 10:53:20 +0000

    update license

--- libc/signal/raise.c
+++ libc/signal/raise.c
@@ -1,6 +1,5 @@
 /* Copyright (C) 1995,1996 Robert de Bath <rdebath@cix.compulink.co.uk>
- * This file is part of the Linux-8086 C library and is distributed
- * under the GNU Library General Public License.
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
 #include <unistd.h>
