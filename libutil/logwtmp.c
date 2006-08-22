fcf7594952bc345864ad66791bc582293770d5c0
"Steven J. Hill" <sjhill@realitydiluted.com>
2006-08-22 02:19:51 +0000

    Merge from trunk.

--- libutil/logwtmp.c
+++ libutil/logwtmp.c
@@ -1,22 +1,9 @@
-/* wtmp support rubbish (i.e. complete crap)
-
-   Written by Erik Andersen <andersee@debian.org> 
-
-   The GNU C Library is free software
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
+/* vi: set sw=4 ts=4: */
+/*
+ * wtmp support rubbish (i.e. complete crap)
+ * Copyright (C) 2000-2006 by Erik Andersen <andersen@uclibc.org>
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
 
 #include <string.h>
 #include <sys/time.h>
