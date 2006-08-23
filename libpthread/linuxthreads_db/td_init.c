60907d3b8554695b78e5517df14e70064290f28c
Mike Frysinger <vapier@gentoo.org>
2006-08-23 15:28:48 +0000

    pull from upstream

--- libpthread/linuxthreads_db/td_init.c
+++ libpthread/linuxthreads_db/td_init.c
@@ -1,5 +1,5 @@
 /* Initialization function of thread debugger support library.
-   Copyright (C) 1999, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2001, 2006 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1999.
 
@@ -23,6 +23,7 @@
 #ifndef NDEBUG
 int __td_debug;
 #endif
+
 
 td_err_e
 td_init (void)
