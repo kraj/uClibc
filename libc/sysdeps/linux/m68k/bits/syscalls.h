27128f302fbb564e9d48770568612185e2c9f06b
Mike Frysinger <vapier@gentoo.org>
2006-08-24 19:47:34 +0000

    only pull in errno.h when not assembler

--- libc/sysdeps/linux/m68k/bits/syscalls.h
+++ libc/sysdeps/linux/m68k/bits/syscalls.h
@@ -4,8 +4,6 @@
 # error "Never use <bits/syscalls.h> directly; include <sys/syscall.h> instead."
 #endif
 
-#include <errno.h>
-
 /* This includes the `__NR_<name>' syscall numbers taken from the Linux kernel
  * header files.  It also defines the traditional `SYS_<name>' macros for older
  * programs.  */
@@ -19,6 +17,8 @@
 #undef __NR_vm86
 
 #ifndef __ASSEMBLER__
+
+#include <errno.h>
 
 /* Linux takes system call arguments in registers:
 
