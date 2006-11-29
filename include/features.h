2c20597657d269d5f61592278d589dd35cd040f2
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-11-29 21:10:04 +0000

    - honor __UCLIBC_HAS_LFS__

--- include/features.h
+++ include/features.h
@@ -176,8 +176,10 @@
 # define _XOPEN_SOURCE	600
 # undef  _XOPEN_SOURCE_EXTENDED
 # define _XOPEN_SOURCE_EXTENDED	1
-# undef	 _LARGEFILE64_SOURCE
-# define _LARGEFILE64_SOURCE	1
+# ifdef __UCLIBC_HAS_LFS__
+#  undef	 _LARGEFILE64_SOURCE
+#  define _LARGEFILE64_SOURCE	1
+# endif /* __UCLIBC_HAS_LFS__ */
 # undef  _BSD_SOURCE
 # define _BSD_SOURCE	1
 # undef  _SVID_SOURCE
