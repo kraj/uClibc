14452ff798b09b3f1615769041369c04219c49ed
Mike Frysinger <vapier@gentoo.org>
2007-02-21 16:14:11 +0000

    add missing file for previous commit as pointed out by Peter Kjellerstedt

---
+++ libc/misc/internals/internal_errno.h
@@ -0,0 +1,21 @@
+/*
+ *
+ */
+
+#include <features.h>
+#include <errno.h>
+#include <netdb.h>
+
+#undef errno
+#undef h_errno
+
+#ifdef __UCLIBC_HAS_THREADS__
+# define errno __uclibc_errno
+# define h_errno __uclibc_h_errno
+#endif
+
+extern int h_errno;
+libc_hidden_proto(h_errno)
+
+extern int errno;
+libc_hidden_proto(errno)
