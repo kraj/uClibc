de853823e0f43825ca25572cdf254b57b4345822
Mike Frysinger <vapier@gentoo.org>
2007-02-22 17:23:56 +0000

    need to double check internal errno handling wrt libpthread

--- libc/misc/internals/internal_errno.h
+++ libc/misc/internals/internal_errno.h
@@ -9,10 +9,12 @@
 #undef errno
 #undef h_errno
 
+/* need to tweak libpthread
 #ifdef __UCLIBC_HAS_THREADS__
 # define errno __uclibc_errno
 # define h_errno __uclibc_h_errno
 #endif
+*/
 
 extern int h_errno;
 libc_hidden_proto(h_errno)
