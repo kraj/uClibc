a7f47450c0e41dc209132b614d36bd58041ec00d
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-06-24 17:23:54 +0000

    - make sure to define _LIBC in libc-symbols.h before including anything   else. - internal_function is only used inside libc, so do not define it in   uClibc_arch_features.h on i386 if we are outside of libc.

--- libc/sysdeps/linux/i386/bits/uClibc_arch_features.h
+++ libc/sysdeps/linux/i386/bits/uClibc_arch_features.h
@@ -42,6 +42,8 @@
 /* define if target supports IEEE signed zero floats */
 #define __UCLIBC_HAVE_SIGNED_ZERO__
 
+#if defined _LIBC
 #define internal_function __attribute__ ((regparm (3), stdcall))
+#endif
 
 #endif /* _BITS_UCLIBC_ARCH_FEATURES_H */
