f985c8316e0ee82b45ae9f75dfe4a02b066b7388
Mike Frysinger <vapier@gentoo.org>
2008-01-05 07:18:36 +0000

    blah, enable the stupid hidden proto for fcntl

--- libc/misc/syslog/syslog.c
+++ libc/misc/syslog/syslog.c
@@ -91,7 +91,7 @@ libc_hidden_proto(strchr)
 libc_hidden_proto(strlen)
 libc_hidden_proto(strncpy)
 libc_hidden_proto(open)
-/*libc_hidden_proto(fcntl)*/
+libc_hidden_proto(fcntl)
 libc_hidden_proto(socket)
 libc_hidden_proto(close)
 libc_hidden_proto(write)
