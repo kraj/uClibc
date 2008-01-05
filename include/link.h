12744f144e1c18ea87aea8cfb7acf5e9f0c78dc3
Mike Frysinger <vapier@gentoo.org>
2008-01-05 17:54:32 +0000

    Merge r20475 by bernds from trunk: Certain configure scripts test for presence of dlfcn.h.  Don't install it if !HAVE_SHARED.

--- include/link.h
+++ include/link.h
@@ -23,7 +23,9 @@
 
 #include <features.h>
 #include <elf.h>
+#ifndef __HAVE_NO_SHARED__
 #include <dlfcn.h>
+#endif
 #include <sys/types.h>
 #if defined _LIBC && defined __UCLIBC_HAS_THREADS_NATIVE__
 #include <tls.h>
