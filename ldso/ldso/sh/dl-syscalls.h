5952806e050a8494cdff3368b144de206def8780
Carmelo Amoroso <carmelo.amoroso@st.com>
2008-01-12 17:59:10 +0000

    Make sh4 build works again adding a temporary work-around iby redefining __always_inline to inline until gcc 4.x.x will get fixed.
    Signed-off-by: Carmelo Amoroso <carmelo.amoroso@st.com>

--- ldso/ldso/sh/dl-syscalls.h
+++ ldso/ldso/sh/dl-syscalls.h
@@ -4,8 +4,11 @@
 extern int _dl_errno;
 #undef __set_errno
 #define __set_errno(X) {(_dl_errno) = (X);}
-#warning !!! __always_inline redefined waiting for the fixed gcc
-#ifdef __always_inline
-#undef __always_inline
-#define __always_inline inline
+
+#if __GNUC_PREREQ (4, 1)
+#warning !!! gcc 4.1 and later have problems with __always_inline so redefined as inline
+# ifdef __always_inline
+# undef __always_inline
+# define __always_inline inline
+# endif
 #endif
