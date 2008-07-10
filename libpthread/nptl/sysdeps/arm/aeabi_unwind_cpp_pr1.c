e3c51bfa967f849292c220b4ae7f19089682f59b
Khem Raj <kraj@mvista.com>
2008-07-10 01:06:46 +0000

    ARM specific NPTL bits

---
+++ libpthread/nptl/sysdeps/arm/aeabi_unwind_cpp_pr1.c
@@ -0,0 +1,1 @@
+#include <../../../../libc/sysdeps/linux/arm/aeabi_unwind_cpp_pr1.c>
---
+++ libpthread/nptl/sysdeps/unix/sysv/linux/arm/pt-gettimeofday.c
@@ -0,0 +1,6 @@
+#include "../../../../../../../libc/sysdeps/linux/common/syscalls.h"
+#include <sys/time.h>
+
+int gettimeofday (struct timeval *, struct timezone *) attribute_hidden;
+_syscall2(int, gettimeofday, struct timeval *, tv, struct timezone *, tz);
+
