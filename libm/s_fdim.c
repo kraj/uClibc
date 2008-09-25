c7a58c2edadb8fce84ab2f8b129a87eb19bdc485
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-09-25 19:07:18 +0000

    - add some more math functions (patch from gentoo/solar)

---
+++ libm/s_fdim.c
@@ -0,0 +1,25 @@
+/* Copyright (C) 2002 by  Red Hat, Incorporated. All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software
+ * is freely granted, provided that this notice is preserved.
+ */
+
+#include "math.h"
+#include "math_private.h"
+
+libm_hidden_proto(fdim)
+#ifdef __STDC__
+	double fdim(double x, double y)
+#else
+	double fdim(x,y)
+	double x;
+	double y;
+#endif
+{
+  int c = __fpclassify(x);
+  if (c == FP_NAN || c == FP_INFINITE)
+    return HUGE_VAL;
+
+  return x > y ? x - y : 0.0;
+}
+libm_hidden_def(fdim)
