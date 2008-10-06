d25d39b7ef9440165a077fe1367b7d662022397c
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-10-06 10:24:41 +0000

    - implement log2()

---
+++ libm/w_log2.c
@@ -0,0 +1,14 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * Copyright (C) 2008 by Bernhard Reutner-Fischer <uclibc@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+#include <math.h>
+#include "math_private.h"
+
+double log2 (double d)
+{
+	return __ieee754_log2 (d);
+}
+libm_hidden_def(log2)
