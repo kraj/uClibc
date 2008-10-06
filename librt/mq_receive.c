c0adda8b79ffe068586600f0a17dfa8a03a4c247
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-10-06 08:55:23 +0000

    - disentangle REALTIME from ADVANCED_REALTIME (Peter S. Mazinger)

--- librt/mq_receive.c
+++ librt/mq_receive.c
@@ -6,16 +6,13 @@
 #include <stddef.h>
 #include <sys/syscall.h>
 #include <mqueue.h>
-#warning FIXME: hard dependency on ADVANCED REALTIME feature
 
-librt_hidden_proto(mq_timedreceive)
 #ifdef __NR_mq_timedreceive
 #define __NR___syscall_mq_timedreceive __NR_mq_timedreceive
-static __inline__ _syscall5(int, __syscall_mq_timedreceive, int, mqdes,
-			char *, msg_ptr, size_t, msg_len, unsigned int *,
-			msg_prio, const void *, abs_timeout);
-#endif
-
+static _syscall5(int, __syscall_mq_timedreceive, int, mqdes,
+		 char *, msg_ptr, size_t, msg_len, unsigned int *,
+		 msg_prio, const void *, abs_timeout)
+# if defined __USE_XOPEN2K && defined __UCLIBC_HAS_ADVANCED_REALTIME__
 /*
  * Receive the oldest from highest priority messages.
  * Stop waiting if abs_timeout expires.
@@ -24,20 +21,31 @@ ssize_t mq_timedreceive(mqd_t mqdes, char *msg_ptr, size_t msg_len,
 			unsigned int *msg_prio,
 			const struct timespec *abs_timeout)
 {
-#ifdef __NR_mq_timedreceive
 	return __syscall_mq_timedreceive(mqdes, msg_ptr, msg_len, msg_prio,
 					 abs_timeout);
-#else
-	errno = ENOSYS;
-	return -1;
-#endif
 }
-
-librt_hidden_def(mq_timedreceive)
+# endif
 
 /* Receive the oldest from highest priority messages */
 ssize_t mq_receive(mqd_t mqdes, char *msg_ptr, size_t msg_len,
 		   unsigned int *msg_prio)
 {
-	return mq_timedreceive(mqdes, msg_ptr, msg_len, msg_prio, NULL);
+	return __syscall_mq_timedreceive(mqdes, msg_ptr, msg_len, msg_prio, NULL);
 }
+#elif defined __UCLIBC_HAS_STUBS__
+# if defined __USE_XOPEN2K && defined __UCLIBC_HAS_ADVANCED_REALTIME__
+ssize_t mq_timedreceive(mqd_t mqdes attribute_unused, char *msg_ptr attribute_unused,
+			size_t msg_len attribute_unused, unsigned int *msg_prio attribute_unused,
+			const struct timespec *abs_timeout attribute_unused)
+{
+	__set_errno(ENOSYS);
+	return -1;
+}
+# endif
+ssize_t mq_receive(mqd_t mqdes attribute_unused, char *msg_ptr attribute_unused,
+		   size_t msg_len attribute_unused, unsigned int *msg_prio attribute_unused)
+{
+	__set_errno(ENOSYS);
+	return -1;
+}
+#endif
