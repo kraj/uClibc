a4f317cd686dee9617cc69c2e9207b3b8d18cdc0
Carmelo Amoroso <carmelo.amoroso@st.com>
2008-11-18 13:31:13 +0000

    Changed Copyright and License statement according to COPYING.LIB.boilerplate

--- libc/misc/elf/dl-iterate-phdr.c
+++ libc/misc/elf/dl-iterate-phdr.c
@@ -1,23 +1,15 @@
 /* Get loaded objects program headers.
-   Copyright (C) 2001,2002,2003,2004,2006,2007 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-   Contributed by Jakub Jelinek <jakub@redhat.com>, 2001.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License as
-   published by the Free Software Foundation; either version 2.1 of the
-   License, or (at your option) any later version.
+  Based on GNU C library (file: libc/elf/dl-iteratephdr.c)
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+  Copyright (C) 2001,2002,2003,2004,2006,2007 Free Software Foundation, Inc.
+  Contributed by Jakub Jelinek <jakub@redhat.com>, 2001.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-   */
+  Copyright (C) 2008 STMicroelectronics Ltd.
+  Author: Carmelo Amoroso <carmelo.amoroso@st.com>
+
+  Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+*/
 
 
 #include <link.h>
