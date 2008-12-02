9c936bc66adc1d437671d9a6ecc06f15614b1942
Khem Raj <kraj@mvista.com>
2008-12-02 06:38:26 +0000

    signed-off-by: Khem Raj <raj.khem@gmail.com> Sync from trunk.

--- libc/misc/internals/__h_errno_location.c
+++ libc/misc/internals/__h_errno_location.c
@@ -1,14 +1,14 @@
-#define __FORCE_GLIBC
-#include <features.h>
-#include <netdb.h>
+/*
+ * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
 
-#ifndef __UCLIBC_HAS_THREADS_NATIVE__
-#undef h_errno
-extern int h_errno;
-#endif
+#include "internal_errno.h"
 
+/* libc_hidden_proto(__h_errno_location) */
 int * weak_const_function __h_errno_location (void)
 {
     return &h_errno;
 }
-
+libc_hidden_weak(__h_errno_location)
