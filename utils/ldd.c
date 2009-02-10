d8a9cc5b95993901632a91302dbb5d0738169d8a
Mike Frysinger <vapier@gentoo.org>
2009-02-10 04:10:34 +0000

    include more info in error messages so we have a better idea where things are failing
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- utils/ldd.c
+++ utils/ldd.c
@@ -367,7 +367,7 @@ static void search_for_named_library(char *name, char *result,
 	/* We need a writable copy of this string */
 	path = strdup(path_list);
 	if (!path) {
-		fprintf(stderr, "Out of memory!\n");
+		fprintf(stderr, "%s: Out of memory!\n", __func__);
 		exit(EXIT_FAILURE);
 	}
 	/* Eliminate all double //s */
@@ -416,7 +416,7 @@ void locate_library_file(ElfW(Ehdr) *ehdr, ElfW(Dyn) *dynamic, int is_suid,
 	/* We need some elbow room here.  Make some room... */
 	buf = malloc(1024);
 	if (!buf) {
-		fprintf(stderr, "Out of memory!\n");
+		fprintf(stderr, "%s: Out of memory!\n", __func__);
 		exit(EXIT_FAILURE);
 	}
 
@@ -673,7 +673,7 @@ int find_dependancies(char *filename)
 	ehdr = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fileno(thefile), 0);
 	if (ehdr == MAP_FAILED) {
 		fclose(thefile);
-		fprintf(stderr, "Out of memory!\n");
+		fprintf(stderr, "mmap(%s) failed: %s\n", filename, strerror(errno));
 		return -1;
 	}
 
