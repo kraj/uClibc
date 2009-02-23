1a08441c06b25efb87de33512674e80a8dedc63e
Carmelo Amoroso <carmelo.amoroso@st.com>
2009-02-23 07:00:33 +0000

    Use gcc built-in defines for detecting SH cores instead of uclibc config option macros (aligning SH to all other archs).
    Changes applied as below:
    __CONFIG_SH4__  -> __SH4__
    __CONFIG_SH2__  -> __sh2__
    __CONFIG_SH2A__ -> __SH2A__
    
    Note: pay attention to capital letters
    
    Signed-off-by: Carmelo Amoroso <carmelo.amoroso@st.com>

--- libc/sysdeps/linux/sh/bits/syscalls.h
+++ libc/sysdeps/linux/sh/bits/syscalls.h
@@ -5,7 +5,7 @@
 #endif
 
 /* The Linux kernel uses different trap numbers on sh-2.  */
-#if defined __CONFIG_SH2__ || defined __CONFIG_SH2A__
+#if defined __sh2__ || defined __SH2A__
 # define __SH_SYSCALL_TRAP_BASE 0x20
 #else
 # define __SH_SYSCALL_TRAP_BASE 0x10
