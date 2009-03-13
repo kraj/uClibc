b29e90d81dead918ee0e5e63fdc39ba2e921ad03
Mike Frysinger <vapier@gentoo.org>
2009-03-13 06:17:48 +0000

    add GNU extension for select timeouts where the sub-second field is actually longer than one second
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/common/select.c
+++ libc/sysdeps/linux/common/select.c
@@ -9,8 +9,11 @@
 
 #include <sys/syscall.h>
 #include <sys/select.h>
+#include <stdint.h>
 
 extern __typeof(select) __libc_select;
+
+#define USEC_PER_SEC 1000000L
 
 #if !defined(__NR__newselect) && !defined(__NR_select) && defined __USE_XOPEN2K
 # define __NR___libc_pselect6 __NR_pselect6
@@ -23,10 +26,26 @@ int __libc_select(int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
 {
 	struct timespec _ts, *ts = 0;
 	if (timeout) {
+		uint32_t usec;
 		_ts.tv_sec = timeout->tv_sec;
-		_ts.tv_nsec = timeout->tv_usec * 1000;
+
+		/* GNU extension: allow for timespec values where the sub-sec
+		 * field is equal to or more than 1 second.  The kernel will
+		 * reject this on us, so take care of the time shift ourself.
+		 * Some applications (like readline and linphone) do this.
+		 * See 'clarification on select() type calls and invalid timeouts'
+		 * on the POSIX general list for more information.
+		 */
+		usec = timeout->tv_usec;
+		if (usec >= USEC_PER_SEC) {
+			_ts.tv_sec += usec / USEC_PER_SEC;
+			usec %= USEC_PER_SEC;
+		}
+		_ts.tv_nsec = usec * 1000;
+
 		ts = &_ts;
 	}
+
 	return __libc_pselect6(n, readfds, writefds, exceptfds, ts, 0);
 }
 
