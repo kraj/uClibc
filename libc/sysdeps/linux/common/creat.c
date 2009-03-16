74cc326ef4e35360b4c22993b964525456bb91ac
Khem Raj <kraj@mvista.com>
2009-03-16 06:02:47 +0000

    Update copyright header.

--- libc/sysdeps/linux/common/creat.c
+++ libc/sysdeps/linux/common/creat.c
@@ -2,7 +2,7 @@
 /*
  * creat() for uClibc
  *
- * Copyright (C) 2009 <kraj@uclibc.org>
+ * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
  *
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
