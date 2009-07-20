e77fe036cfbcb690d4b2f19d58665bb28d00e4a0
Mike Frysinger <vapier@gentoo.org>
2009-07-20 16:38:30 -0400

    byteswap: unify common definitions
    The majority of the byteswap functions are the same across all arches, so
    setup a common header to provide definitions if they don't exist.  This
    allows arches to override only the ones they actually want to implement
    with inline assembly.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- libc/sysdeps/linux/h8300/bits/byteswap.h
+++ libc/sysdeps/linux/h8300/bits/byteswap.h
@@ -17,51 +17,24 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#if !defined _BYTESWAP_H && !defined _NETINET_IN_H
-# error "Never use <bits/byteswap.h> directly; include <byteswap.h> instead."
-#endif
+#ifndef _ASM_BITS_BYTESWAP_H
+#define _ASM_BITS_BYTESWAP_H 1
 
-/* Swap bytes in 16 bit value.  We don't provide an assembler version
-   because GCC is smart enough to generate optimal assembler output, and
-   this allows for better cse.  */
-#define __bswap_16(x) \
-  ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
-
-/* Swap bytes in 32 bit value.  */
-#define __bswap_constant_32(x) \
-  ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
-   (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
-
-#if defined __GNUC__ && __GNUC__ >= 2
-# define __bswap_32(x) \
-  __extension__					\
-  ({ unsigned int __v;				\
-     if (__builtin_constant_p (x))		\
-       __v = __bswap_constant_32 (x);		\
-     else					\
-       __asm__ __volatile__ ("mov.l %0,er0\n\t"	\
-			     "mov.b r0l,r1h\n\t"	\
-			     "mov.b r0h,r1l\n\t"	\
-			     "mov.w r1,e1\n\t"	\
-			     "mov.w e0,r0\n\t"	\
-			     "mov.b r0l,r1h\n\t"	\
-			     "mov.b r0h,r1l\n\t"	\
-			     "mov.l er1,%0"	\
-			     : "=d" (__v)	\
-			     : "0" (x): "er0","er1");	\
-     __v; })
-#else
-# define __bswap_32(x) __bswap_constant_32 (x)
-#endif
-
-#if defined __GNUC__ && __GNUC__ >= 2
-/* Swap bytes in 64 bit value.  */
-# define __bswap_64(x) \
+#define __bswap_non_constant_32(x) \
   __extension__						\
-  ({ union { unsigned long long int __ll;		\
-	     unsigned long int __l[2]; } __v, __r;	\
-     __v.__ll = (x);					\
-     __r.__l[0] = __bswap_32 (__v.__l[1]);		\
-     __r.__l[1] = __bswap_32 (__v.__l[0]);		\
-     __r.__ll; })
+  ({ unsigned int __v;					\
+     __asm__ __volatile__ ("mov.l %0,er0\n\t"		\
+			   "mov.b r0l,r1h\n\t"		\
+			   "mov.b r0h,r1l\n\t"		\
+			   "mov.w r1,e1\n\t"		\
+			   "mov.w e0,r0\n\t"		\
+			   "mov.b r0l,r1h\n\t"		\
+			   "mov.b r0h,r1l\n\t"		\
+			   "mov.l er1,%0"		\
+			   : "=d" (__v)			\
+			   : "0" (x): "er0","er1");	\
+     __v; })
+
 #endif
+
+#include <bits/byteswap-common.h>
--- libc/sysdeps/linux/ia64/bits/byteswap.h
+++ libc/sysdeps/linux/ia64/bits/byteswap.h
@@ -17,94 +17,36 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#if !defined _BYTESWAP_H && !defined _NETINET_IN_H
-# error "Never use <bits/byteswap.h> directly; include <byteswap.h> instead."
-#endif
+#ifndef _ASM_BITS_BYTESWAP_H
+#define _ASM_BITS_BYTESWAP_H 1
 
-#ifndef _BITS_BYTESWAP_H
-#define _BITS_BYTESWAP_H 1
-
-/* Swap bytes in 16 bit value.  */
-#define __bswap_constant_16(x) \
-     ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
-
-#if defined __GNUC__ && __GNUC__ >= 2
-# define __bswap_16(x) \
+#define __bswap_non_constant_16(x) \
      (__extension__							      \
-      ({ register unsigned short int __v, __x = (x);			      \
-	 if (__builtin_constant_p (x))					      \
-	   __v = __bswap_constant_16 (__x);				      \
-	 else								      \
-	   __asm__ __volatile__ ("shl %0 = %1, 48 ;;"			      \
-				 "mux1 %0 = %0, @rev ;;"		      \
-				 : "=r" (__v)				      \
-				 : "r" ((unsigned short int) (__x)));	      \
+      ({ register unsigned short int __v;				      \
+	 __asm__ __volatile__ ("shl %0 = %1, 48 ;;"			      \
+				"mux1 %0 = %0, @rev ;;"			      \
+				: "=r" (__v)				      \
+				: "r" ((unsigned short int) (x)));	      \
 	 __v; }))
-#else
-/* This is better than nothing.  */
-static __inline unsigned short int
-__bswap_16 (unsigned short int __bsx)
-{
-  return __bswap_constant_16 (__bsx);
-}
-#endif
 
-
-/* Swap bytes in 32 bit value.  */
-#define __bswap_constant_32(x) \
-     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |		      \
-      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
-
-#if defined __GNUC__ && __GNUC__ >= 2
-# define __bswap_32(x) \
+#define __bswap_non_constant_32(x) \
      (__extension__							      \
-      ({ register unsigned int __v, __x = (x);				      \
-	 if (__builtin_constant_p (x))					      \
-	   __v = __bswap_constant_32 (__x);				      \
-	 else								      \
-	   __asm__ __volatile__ ("shl %0 = %1, 32 ;;"			      \
-				 "mux1 %0 = %0, @rev ;;"		      \
-				 : "=r" (__v)				      \
-				 : "r" ((unsigned int) (__x)));		      \
+      ({ register unsigned int __v;					      \
+	 __asm__ __volatile__ ("shl %0 = %1, 32 ;;"			      \
+				"mux1 %0 = %0, @rev ;;"			      \
+				: "=r" (__v)				      \
+				: "r" ((unsigned int) (x)));		      \
 	 __v; }))
-#else
-static __inline unsigned int
-__bswap_32 (unsigned int __bsx)
-{
-  return __bswap_constant_32 (__bsx);
-}
-#endif
 
-
-/* Swap bytes in 64 bit value.  */
-#define __bswap_constant_64(x) \
-     ((((x) & 0xff00000000000000ul) >> 56)				      \
-      | (((x) & 0x00ff000000000000ul) >>  40)				      \
-      | (((x) & 0x0000ff0000000000ul) >> 24)				      \
-      | (((x) & 0x000000ff00000000ul) >> 8)				      \
-      | (((x) & 0x00000000ff000000ul) << 8)				      \
-      | (((x) & 0x0000000000ff0000ul) << 24)				      \
-      | (((x) & 0x000000000000ff00ul) << 40)				      \
-      | (((x) & 0x00000000000000fful) << 56))
-
-#if defined __GNUC__ && __GNUC__ >= 2
-# define __bswap_64(x) \
+#define __bswap_non_constant_64(x) \
      (__extension__							      \
-      ({ register unsigned long int __v, __x = (x);			      \
-	 if (__builtin_constant_p (x))					      \
-	   __v = __bswap_constant_64 (__x);				      \
-	 else								      \
-	   __asm__ __volatile__ ("mux1 %0 = %1, @rev ;;"		      \
-				 : "=r" (__v)				      \
-				 : "r" ((unsigned long int) (__x)));	      \
+      ({ register unsigned long int __v;				      \
+	 __asm__ __volatile__ ("mux1 %0 = %1, @rev ;;"			      \
+				: "=r" (__v)				      \
+				: "r" ((unsigned long int) (x)));	      \
          __v; }))
-
-#else
-static __inline unsigned long int
-__bswap_64 (unsigned long int __bsx)
-{
-  return __bswap_constant_64 (__bsx);
-}
 #endif
 
-#endif /* _BITS_BYTESWAP_H */
+#endif
+
+#include <bits/byteswap-common.h>
