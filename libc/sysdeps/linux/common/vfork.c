711ad9f92c1cf992c4a3d9f4f709bd692be7789c
Mike Frysinger <vapier@gentoo.org>
2009-07-23 04:03:17 -0400

    create real common vfork() function
    Rather than force people to always implement their own vfork(), have the
    default implementation be sane.
    
    For now, only the Blackfin port uses the new code.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- libc/sysdeps/linux/common/vfork.c
+++ libc/sysdeps/linux/common/vfork.c
@@ -4,24 +4,30 @@
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
-/* Trivial implementation for arches that lack vfork */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
 
-#ifdef __ARCH_USE_MMU__
-
-#ifdef __NR_fork
-/* libc_hidden_proto(fork) */
-
 extern __typeof(vfork) __vfork attribute_hidden;
+
+#ifdef __NR_vfork
+
+# define __NR___vfork __NR_vfork
+_syscall0(pid_t, __vfork)
+
+weak_alias(__vfork,vfork)
+libc_hidden_weak(vfork)
+
+#elif defined __ARCH_USE_MMU__ && defined __NR_fork
+
+/* Trivial implementation for arches that lack vfork */
+
 pid_t __vfork(void)
 {
     return fork();
 }
-/* libc_hidden_proto(vfork) */
+
 weak_alias(__vfork,vfork)
 libc_hidden_weak(vfork)
-#endif
 
 #endif
