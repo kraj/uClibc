31da19a6cb46f9e51744f11008deb76f72e1f673
Denys Vlasenko <vda.linux@googlemail.com>
2009-09-05 23:30:57 +0200

    wtmp code: style fixes, no code changes
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- libutil/logwtmp.c
+++ libutil/logwtmp.c
@@ -13,41 +13,40 @@
 #include <fcntl.h>
 #include <sys/file.h>
 
-
-void logwtmp (const char *line, const char *name, const char *host)
+void logwtmp(const char *line, const char *name, const char *host)
 {
     struct utmp lutmp;
-    memset (&(lutmp), 0, sizeof (struct utmp));
+    memset(&lutmp, 0, sizeof(lutmp));
 
-    lutmp.ut_type = (name && *name)? USER_PROCESS : DEAD_PROCESS;
+    lutmp.ut_type = (name && *name) ? USER_PROCESS : DEAD_PROCESS;
     lutmp.ut_pid = getpid();
     strncpy(lutmp.ut_line, line, sizeof(lutmp.ut_line)-1);
     strncpy(lutmp.ut_name, name, sizeof(lutmp.ut_name)-1);
     strncpy(lutmp.ut_host, host, sizeof(lutmp.ut_host)-1);
 #if !defined __WORDSIZE_COMPAT32 || __WORDSIZE_COMPAT32 == 0
-    gettimeofday(&(lutmp.ut_tv), NULL);
+    gettimeofday(&lutmp.ut_tv, NULL);
 #else
     {
       struct timeval tv;
-      gettimeofday (&tv, NULL);
+      gettimeofday(&tv, NULL);
       lutmp.ut_tv.tv_sec = tv.tv_sec;
       lutmp.ut_tv.tv_usec = tv.tv_usec;
     }
 #endif
 
-    updwtmp(_PATH_WTMP, &(lutmp));
+    updwtmp(_PATH_WTMP, &lutmp);
 }
 
 #if 0
 /* This is enabled in uClibc/libc/misc/utmp/wtent.c */
-extern void updwtmp(const char *wtmp_file, const struct utmp *lutmp)
+void updwtmp(const char *wtmp_file, const struct utmp *lutmp)
 {
     int fd;
 
-    fd = open(wtmp_file, O_APPEND | O_WRONLY, 0);
+    fd = open(wtmp_file, O_APPEND | O_WRONLY);
     if (fd >= 0) {
-	if (lockf(fd, F_LOCK, 0)==0) {
-	    write(fd, (const char *) lutmp, sizeof(struct utmp));
+	if (lockf(fd, F_LOCK, 0) == 0) {
+	    write(fd, lutmp, sizeof(*lutmp));
 	    lockf(fd, F_ULOCK, 0);
 	    close(fd);
 	}
