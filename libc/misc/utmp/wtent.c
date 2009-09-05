45f5c6f25b11fddd66f7a486b988d0d8e7e00341
Denys Vlasenko <vda.linux@googlemail.com>
2009-09-05 23:30:57 +0200

    wtmp code: style fixes, no code changes
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libc/misc/utmp/wtent.c
+++ libc/misc/utmp/wtent.c
@@ -15,31 +15,20 @@
 #include <sys/file.h>
 
 #if 0
-/* Experimentally off - libc_hidden_proto(memset) */
-/* Experimentally off - libc_hidden_proto(strncpy) */
-/* libc_hidden_proto(updwtmp) */
-#endif
-/* libc_hidden_proto(open) */
-/* libc_hidden_proto(write) */
-/* libc_hidden_proto(close) */
-/* libc_hidden_proto(lockf) */
-/* libc_hidden_proto(gettimeofday) */
-
-#if 0
 /* This is enabled in uClibc/libutil/logwtmp.c */
 void logwtmp (const char *line, const char *name, const char *host)
 {
     struct utmp lutmp;
-    memset (&(lutmp), 0, sizeof (struct utmp));
+    memset(&lutmp, 0, sizeof(lutmp));
 
-    lutmp.ut_type = (name && *name)? USER_PROCESS : DEAD_PROCESS;
-    lutmp.ut_pid = __getpid();
+    lutmp.ut_type = (name && *name) ? USER_PROCESS : DEAD_PROCESS;
+    lutmp.ut_pid = getpid();
     strncpy(lutmp.ut_line, line, sizeof(lutmp.ut_line)-1);
     strncpy(lutmp.ut_name, name, sizeof(lutmp.ut_name)-1);
     strncpy(lutmp.ut_host, host, sizeof(lutmp.ut_host)-1);
     gettimeofday(&(lutmp.ut_tv), NULL);
 
-    updwtmp(_PATH_WTMP, &(lutmp));
+    updwtmp(_PATH_WTMP, &lutmp);
 }
 #endif
 
@@ -47,10 +36,10 @@ void updwtmp(const char *wtmp_file, const struct utmp *lutmp)
 {
     int fd;
 
-    fd = open(wtmp_file, O_APPEND | O_WRONLY, 0);
+    fd = open(wtmp_file, O_APPEND | O_WRONLY);
     if (fd >= 0) {
-	if (lockf(fd, F_LOCK, 0)==0) {
-	    write(fd, (const char *) lutmp, sizeof(struct utmp));
+	if (lockf(fd, F_LOCK, 0) == 0) {
+	    write(fd, lutmp, sizeof(*lutmp));
 	    lockf(fd, F_ULOCK, 0);
 	    close(fd);
 	}
