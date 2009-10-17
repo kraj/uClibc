b88b64bd06d9c0945e53a99b6ea840f38358df99
Peter S. Mazinger <ps.m@gmx.net>
2009-10-17 03:31:36 +0200

    use __BYTE_ORDER and __*ENDIAN
    Those without the __ prefix are BSD specific.
    
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- include/arpa/nameser_compat.h
+++ include/arpa/nameser_compat.h
@@ -47,7 +47,7 @@
 
 typedef struct {
 	unsigned	id :16;		/* query identification number */
-#if BYTE_ORDER == BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
 			/* fields in third byte */
 	unsigned	qr: 1;		/* response flag */
 	unsigned	opcode: 4;	/* purpose of message */
@@ -61,7 +61,7 @@ typedef struct {
 	unsigned	cd: 1;		/* checking disabled by resolver */
 	unsigned	rcode :4;	/* response code */
 #endif
-#if BYTE_ORDER == LITTLE_ENDIAN || BYTE_ORDER == PDP_ENDIAN
+#if __BYTE_ORDER == __LITTLE_ENDIAN || __BYTE_ORDER == __PDP_ENDIAN
 			/* fields in third byte */
 	unsigned	rd :1;		/* recursion desired */
 	unsigned	tc :1;		/* truncated message */
