4a689f0b907d4a98582a9c4b7f6be811924db8ee
Austin Foxley <austinf@cetoncorp.com>
2009-10-17 12:59:14 -0700

    some tweaks under libc/ needed for nptl
     * updated kernel-features.h
     * system is provided by pt-system with nptl
     * _exit should do exit_group with nptl
     * tsd tls ptr in libc
     * rt_sigwaitinfo impl added
    
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- libc/sysdeps/linux/common/_exit.c
+++ libc/sysdeps/linux/common/_exit.c
@@ -12,12 +12,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
+#ifdef __UCLIBC_HAS_THREADS_NATIVE__
+#include <sysdep.h>
+#endif
 
 
 void attribute_noreturn _exit(int status)
 {
 	/* The loop is added only to keep gcc happy. */
 	while(1)
+	{
+#ifdef __UCLIBC_HAS_THREADS_NATIVE__
+# ifdef __NR_exit_group
+		INLINE_SYSCALL(exit_group, 1, status);
+# endif
+#endif
 		INLINE_SYSCALL(exit, 1, status);
+	}
 }
 libc_hidden_def(_exit)
