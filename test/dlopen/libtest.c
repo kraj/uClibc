d74a3db4bf9632a474f44cebe7457e23d45027fb
Austin Foxley <austinf@cetoncorp.com>
2009-10-17 11:54:57 -0700

    test/dlopen: use pthread_once directly
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- test/dlopen/libtest.c
+++ test/dlopen/libtest.c
@@ -2,12 +2,10 @@
 #include <pthread.h>
 #include <stdint.h>
 
-extern int __pthread_once(void);
-
 void dltest(uint32_t **value1, uint32_t **value2);
 void dltest(uint32_t **value1, uint32_t **value2)
 {
-	*value1 = (uint32_t *) __pthread_once;
+	*value1 = (uint32_t *) pthread_once;
 	*value2 = (uint32_t *) pthread_self;
 }
 
