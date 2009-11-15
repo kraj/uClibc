0b3be5bb62587afbb718103298b3767ebd6fb4de
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2009-11-15 19:30:00 +0100

    ldso: Add missing newlines to some debug messages
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- ldso/ldso/sh/elfinterp.c
+++ ldso/ldso/sh/elfinterp.c
@@ -243,7 +243,7 @@ _dl_do_reloc (struct elf_resolve *tpnt,struct dyn_elf *scope,
 	}
 #if defined (__SUPPORT_LD_DEBUG__)
 	    if (_dl_debug_reloc && _dl_debug_detail)
-		_dl_dprintf(_dl_debug_file, "\tpatched: %x ==> %x @ %x", old_val, *reloc_addr, reloc_addr);
+		_dl_dprintf(_dl_debug_file, "\tpatched: %x ==> %x @ %x\n", old_val, *reloc_addr, reloc_addr);
 #endif
 
 	return 0;
@@ -280,7 +280,7 @@ _dl_do_lazy_reloc (struct elf_resolve *tpnt, struct dyn_elf *scope,
 	}
 #if defined (__SUPPORT_LD_DEBUG__)
 	if (_dl_debug_reloc && _dl_debug_detail)
-		_dl_dprintf(_dl_debug_file, "\tpatched: %x ==> %x @ %x", old_val, *reloc_addr, reloc_addr);
+		_dl_dprintf(_dl_debug_file, "\tpatched: %x ==> %x @ %x\n", old_val, *reloc_addr, reloc_addr);
 #endif
 	return 0;
 
