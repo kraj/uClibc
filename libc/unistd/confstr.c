c164cf038c522d153bf9ac6c4bddabe0be81a7e1
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2010-04-08 10:33:09 +0200

    confstr: properly stringify version parts
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/unistd/confstr.c
+++ libc/unistd/confstr.c
@@ -52,11 +52,13 @@ size_t confstr (int name, char *buf, size_t len)
       string_len = sizeof("linuxthreads-x.xx");
 # elif defined __UCLIBC_HAS_THREADS_NATIVE__
 #  define __NPTL_VERSION ("NPTL " \
-		#__UCLIBC_MAJOR__ "." \
-		#__UCLIBC_MINOR__ "." \
-		#__UCLIBC_SUBLEVEL__)
+		__stringify(__UCLIBC_MAJOR__) "." \
+		__stringify(__UCLIBC_MINOR__) "." \
+		__stringify(__UCLIBC_SUBLEVEL__))
       string = __NPTL_VERSION;
       string_len = sizeof(__NPTL_VERSION);
+# else
+#  error unable to determine thread impl
 # endif
       break;
 #endif
