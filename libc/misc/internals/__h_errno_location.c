f418f52701de02954c90341e5c9ccdd3f07ccd33
Peter S. Mazinger <ps.m@gmx.net>
2011-03-25 00:49:42 +0100

    errno, h_errno: correct them for non-TLS
    Move h_errno related stuff to separate file.
    Do not hide errno and h_errno for non-TLS, else it keeps being 0.
    Make __[h_]errno_location weak for non-TLS.
    
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/misc/internals/__h_errno_location.c
+++ libc/misc/internals/__h_errno_location.c
@@ -4,10 +4,16 @@
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
-#include "internal_errno.h"
+#include <features.h>
+#include <netdb.h>
+
+#ifndef __UCLIBC_HAS_TLS__
+# undef h_errno
+extern int h_errno;
+#endif
 
 int *__h_errno_location(void)
 {
     return &h_errno;
 }
-libc_hidden_weak(__h_errno_location)
+libc_hidden_def(__h_errno_location)
