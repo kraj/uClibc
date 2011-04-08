657599bc8ca5c2f0a6bcc6a717b04091c978cd08
Peter S. Mazinger <ps.m@gmx.net>
2011-04-08 15:19:44 +0200

    libdl.c: initialize _dl_loaded_modules for !SHARED
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

--- ldso/libdl/libdl.c
+++ ldso/libdl/libdl.c
@@ -48,6 +48,7 @@ size_t _dl_pagesize            = PAGE_SIZE; /* Store the page size for later use
 /* This global variable is also to communicate with debuggers such as gdb. */
 struct r_debug *_dl_debug_addr = NULL;
 
+#include "../ldso/dl-symbols.c"
 #include "../ldso/dl-array.c"
 #include "../ldso/dl-debug.c"
 
