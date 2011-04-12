c277cc3c99a2499183fed84cdeb25c45a06f141d
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2011-04-12 09:39:20 +0200

    resolv: fix res_close not to hang with ipv6
    Timo Teräs writes:
    The memory release loop is missing an obvious counter increment.
    
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/inet/resolv.c
+++ libc/inet/resolv.c
@@ -3008,7 +3008,7 @@ void res_close(void)
 		char *p1 = (char*) &(_res.nsaddr_list[0]);
 		int m = 0;
 		/* free nsaddrs[m] if they do not point to nsaddr_list[x] */
-		while (m < ARRAY_SIZE(_res._u._ext.nsaddrs)) {
+		while (m++ < ARRAY_SIZE(_res._u._ext.nsaddrs)) {
 			char *p2 = (char*)(_res._u._ext.nsaddrs[m]);
 			if (p2 < p1 || (p2 - p1) > sizeof(_res.nsaddr_list))
 				free(p2);
