f93ebe521c927723f052baade0b7519aee9d2643
Peter S. Mazinger <ps.m@gmx.net>
2011-04-16 02:07:22 +0200

    prctl.c: no need to include stdarg.h and extern
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

--- libc/sysdeps/linux/common/prctl.c
+++ libc/sysdeps/linux/common/prctl.c
@@ -8,10 +8,9 @@
  */
 
 #include <sys/syscall.h>
-#include <stdarg.h>
 /* psm: including sys/prctl.h would depend on kernel headers */
 
 #ifdef __NR_prctl
-extern int prctl (int, long, long, long, long);
+int prctl (int, long, long, long, long);
 _syscall5(int, prctl, int, option, long, _a2, long, _a3, long, _a4, long, _a5)
 #endif
