29f2e0cd6d48770e516f0e7fed333643360defd9
Peter S. Mazinger <ps.m@gmx.net>
2011-04-16 12:42:50 +0200

    sigsuspend.c: make build fail, if rt_sigsuspend is not available and NPTL is defined
    Rework a bit for less code.
    always_inline syscall used only once, do not inline syscall used twice.
    
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/common/sigsuspend.c
+++ libc/sysdeps/linux/common/sigsuspend.c
@@ -9,48 +9,41 @@
 
 #include <sys/syscall.h>
 
-#if defined __USE_POSIX
+#ifdef __USE_POSIX
 #include <signal.h>
-#undef sigsuspend
 
-libc_hidden_proto(sigsuspend)
+#ifdef __UCLIBC_HAS_THREADS_NATIVE__
+# ifndef __NR_rt_sigsuspend
+#  error break build, NPTL needs rt_sigsuspend syscall
+# endif
+# include <sysdep-cancel.h>
+#else
+# define SINGLE_THREAD_P 1
+#endif
 
 #ifdef __NR_rt_sigsuspend
-# define __NR___rt_sigsuspend __NR_rt_sigsuspend
-
-# ifdef __UCLIBC_HAS_THREADS_NATIVE__
-#  include <errno.h>
-#  include <sysdep-cancel.h>
+static _syscall2(int, rt_sigsuspend, const sigset_t *, set, size_t, size)
 
 /* Change the set of blocked signals to SET,
    wait until a signal arrives, and restore the set of blocked signals.  */
-int sigsuspend (const sigset_t *set)
+int sigsuspend(const sigset_t *set)
 {
 	if (SINGLE_THREAD_P)
-		return INLINE_SYSCALL (rt_sigsuspend, 2, set, _NSIG / 8);
+		return rt_sigsuspend(set, _NSIG / 8);
 
+# ifdef __UCLIBC_HAS_THREADS_NATIVE__
 	int oldtype = LIBC_CANCEL_ASYNC ();
-
-	int result = INLINE_SYSCALL (rt_sigsuspend, 2, set, _NSIG / 8);
-
+	int result = rt_sigsuspend(set, _NSIG / 8);
 	LIBC_CANCEL_RESET (oldtype);
-
 	return result;
-}
-# else
-static inline _syscall2(int, __rt_sigsuspend, const sigset_t *, mask, size_t, size)
-
-int sigsuspend(const sigset_t * mask)
-{
-	return __rt_sigsuspend(mask, _NSIG / 8);
-}
 # endif
+}
 #else
 # define __NR___syscall_sigsuspend __NR_sigsuspend
-static __inline__ _syscall3(int, __syscall_sigsuspend, int, a, unsigned long int, b,
-		  unsigned long int, c)
+static __always_inline _syscall3(int, __syscall_sigsuspend, int, a, unsigned long int, b,
+				 unsigned long int, c)
 
-int sigsuspend(const sigset_t * set)
+int sigsuspend(const sigset_t *set)
 {
 	return __syscall_sigsuspend(0, 0, set->__val[0]);
 }
