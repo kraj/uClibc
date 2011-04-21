2c917b0305102f3e01e825406f0e15cd95560312
Peter S. Mazinger <ps.m@gmx.net>
2011-04-21 22:04:49 +0200

    adapt fdatasync, fsync, msync to use cancel.h
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/common/fdatasync.c
+++ libc/sysdeps/linux/common/fdatasync.c
@@ -8,36 +8,17 @@
  */
 
 #include <sys/syscall.h>
-#include <unistd.h>
 
 #if !defined __NR_fdatasync && defined __NR_osf_fdatasync
 # define __NR_fdatasync __NR_osf_fdatasync
 #endif
 
 #ifdef __NR_fdatasync
+# include <unistd.h>
+# include <cancel.h>
 
-# ifdef __UCLIBC_HAS_THREADS_NATIVE__
-# include <sysdep-cancel.h>
-# else
-# define SINGLE_THREAD_P 1
-# endif
+# define __NR___fdatasync_nocancel __NR_fdatasync
+static _syscall1(int, __NC(fdatasync), int, fd)
 
-#define __NR___syscall_fdatasync __NR_fdatasync
-
-static __always_inline
-_syscall1(int, __syscall_fdatasync, int, fd)
-
-int fdatasync(int fd)
-{
-	if (SINGLE_THREAD_P)
-		return __syscall_fdatasync(fd);
-
-# ifdef __UCLIBC_HAS_THREADS_NATIVE__
-	int oldtype = LIBC_CANCEL_ASYNC ();
-	int result = __syscall_fdatasync(fd);
-	LIBC_CANCEL_RESET (oldtype);
-	return result;
-# endif
-}
-
+CANCELLABLE_SYSCALL(int, fdatasync, (int fd), (fd))
 #endif
