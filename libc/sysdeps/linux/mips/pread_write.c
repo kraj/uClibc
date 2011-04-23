944ae666f4262e99ad1d98537a73f2417ec78750
Peter S. Mazinger <ps.m@gmx.net>
2011-04-23 14:13:59 +0200

    mips/pread_write.c: fix typo
    Reported-by: Tony Wu <tung7970@gmail.com>
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

--- libc/sysdeps/linux/mips/pread_write.c
+++ libc/sysdeps/linux/mips/pread_write.c
@@ -26,9 +26,9 @@ static _syscall4(ssize_t, __syscall_pread, int, fd, void *, buf, size_t, count, 
 #  define __NR___syscall_pread __NR_pread
 static _syscall6(ssize_t, __syscall_pread, int, fd, void *, buf,
 		 size_t, count, int, dummy, off_t, offset_hi, off_t, offset_lo)
-#  define MY_PREAD(fd, buf, count, offset)
+#  define MY_PREAD(fd, buf, count, offset) \
 	__syscall_pread(fd, buf, count, 0, OFF_HI_LO(offset))
-#  define MY_PREAD64(fd, buf, count, offset)
+#  define MY_PREAD64(fd, buf, count, offset) \
 	__syscall_pread(fd, buf, count, 0, OFF64_HI_LO(offset))
 # endif
 #endif
@@ -49,9 +49,9 @@ static _syscall4(ssize_t, __syscall_pwrite, int, fd, const void *, buf, size_t, 
 #  define __NR___syscall_pwrite __NR_pwrite
 static _syscall6(ssize_t, __syscall_pwrite, int, fd, const void *, buf,
 		 size_t, count, int, dummy, off_t, offset_hi, off_t, offset_lo)
-#  define MY_PWRITE(fd, buf, count, offset)
+#  define MY_PWRITE(fd, buf, count, offset) \
 	__syscall_pwrite(fd, buf, count, 0, OFF_HI_LO(offset))
-#  define MY_PWRITE64(fd, buf, count, offset)
+#  define MY_PWRITE64(fd, buf, count, offset) \
 	__syscall_pwrite(fd, buf, count, 0, OFF64_HI_LO(offset))
 # endif
 #endif
