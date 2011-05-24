bb3f3b9b555c268bf020e36a898e69bb8403772b
Peter S. Mazinger <ps.m@gmx.net>
2011-05-24 20:51:10 +0200

    cancel.h: add copyright
    Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

--- include/cancel.h
+++ include/cancel.h
@@ -1,3 +1,10 @@
+/* vi: set sw=4 ts=4: */
+/*
+ * Copyright (C) 2000-2011 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+
 #ifndef _CANCEL_H
 #define _CANCEL_H
 
