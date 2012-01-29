46aa707fc50415e90b0860258f4f5d78b7b96a7b
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2012-01-29 15:10:01 +0100

    test: ignore undefined symbols for API
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- test/API/tst-API.sh
+++ test/API/tst-API.sh
@@ -8,8 +8,9 @@ result=0
 
 for l in $top_builddir/lib/lib*-*.so; do \
 	readelf -D -W -s $l | \
-	egrep -v "(UND|LOCAL|^Symbol table for image:|^[[:space:]]*Num[[:space:]])" | \
-	$AWK '{print $NF}'; \
+	$AWK '
+/^[[:space:]]*[[:digit:]]/ { if ($8 != "UND") print $NF; }
+'; \
 	done | sort | uniq > $uclibc_out
 for code in $cod; do cat $code.$std.syms; done | sort | uniq > $glibc_out
 result=0
