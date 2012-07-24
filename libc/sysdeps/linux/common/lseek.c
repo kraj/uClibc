0e28f4b8908d69640fde017ec38cefaebe020f27
Bernd Schmidt <bernds@codesourcery.com>
2012-07-24 15:41:01 +0200

    Accept either __NR__lseek or __NR_lseek
    In llseek.c, we already allow both variants; the same needs to be done here
    to avoid duplicate definitions of lseek64_nocancel.
    
    Signed-off-by: Bernd Schmidt <bernds@codesourcery.com>

--- libc/sysdeps/linux/common/lseek.c
+++ libc/sysdeps/linux/common/lseek.c
@@ -39,7 +39,7 @@ off_t __NC(lseek)(int fd, off_t offset attribute_unused, int whence)
 #endif
 CANCELLABLE_SYSCALL(off_t, lseek, (int fd, off_t offset, int whence), (fd, offset, whence))
 lt_libc_hidden(lseek)
-#if defined __UCLIBC_HAS_LFS__ && (__WORDSIZE == 64 || !defined __NR__llseek)
+#if defined __UCLIBC_HAS_LFS__ && (__WORDSIZE == 64 || (!defined __NR__llseek && !defined __NR_llseek))
 strong_alias_untyped(__NC(lseek),__NC(lseek64))
 strong_alias_untyped(lseek,lseek64)
 lt_libc_hidden(lseek64)
