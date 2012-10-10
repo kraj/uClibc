80dc2ed052dc9a52080ed04bd946f3a537492f24
Markos Chandras <markos.chandras@imgtec.com>
2012-10-10 16:06:57 +0100

    utimes: Use utimensat if arch does not have the utimes syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/common/utimes.c
+++ libc/sysdeps/linux/common/utimes.c
@@ -10,7 +10,26 @@
 #include <sys/syscall.h>
 #include <sys/time.h>
 
-#ifdef __NR_utimes
+#if defined __NR_utimensat && !defined __NR_utimes
+# include <fcntl.h>
+# include <stddef.h>
+int utimes(const char *file, const struct timeval tvp[2])
+{
+	struct timespec ts[2], *times;
+	if (tvp) {
+		times = ts;
+		times[0].tv_sec = tvp[0].tv_sec;
+		times[0].tv_nsec = tvp[0].tv_usec * 1000;
+		times[1].tv_sec = tvp[1].tv_sec;
+		times[1].tv_nsec = tvp[1].tv_usec * 1000;
+	} else {
+		times = NULL;
+	}
+
+	return utimensat(AT_FDCWD, file, times, 0);
+}
+
+#elif defined __NR_utimes
 _syscall2(int, utimes, const char *, file, const struct timeval *, tvp)
 #elif defined __NR_utime
 # define __need_NULL
@@ -31,6 +50,7 @@ int utimes(const char *file, const struct timeval tvp[2])
 	return utime(file, times);
 }
 #endif
+
 #if defined __NR_utimes || defined __NR_utime
 libc_hidden_def(utimes)
 #endif
