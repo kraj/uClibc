dde074b3905cce833e1cdca591174454e046083e
Markos Chandras <markos.chandras@imgtec.com>
2012-10-10 16:24:43 +0100

    pipe: Use pipe2 if arch does not have the pipe syscall
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/common/pipe.c
+++ libc/sysdeps/linux/common/pipe.c
@@ -11,5 +11,13 @@
 #include <unistd.h>
 
 
+#if defined __NR_pipe2 && !defined __NR_pipe
+int pipe(int filedes[2])
+{
+	return pipe2(filedes, 0);
+}
+/* If both are defined then use the pipe syscall */
+#else
 _syscall1(int, pipe, int *, filedes)
+#endif
 libc_hidden_def(pipe)
