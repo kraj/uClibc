608e01aa0d744fb7a486d48da68c9c7355dcd4ea
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2013-02-10 17:21:15 +0100

    stdlib: Fix fndecl of mkostemp, mkostemp64
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/stdlib/mkostemp.c
+++ libc/stdlib/mkostemp.c
@@ -24,9 +24,7 @@
    they are replaced with a string that makes the filename unique.
    Then open the file and return a fd. */
 int
-mkostemp (template, flags)
-     char *template;
-     int flags;
+mkostemp (char *template, int flags)
 {
   return __gen_tempname (template, __GT_FILE, flags);
 }
