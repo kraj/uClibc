7598eeaa1defa2884adaa890bb115c493d69cc35
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2013-02-23 07:39:41 +0100

    test: cater for config
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- test/misc/tst-statfs.c
+++ test/misc/tst-statfs.c
@@ -1,5 +1,3 @@
-#define _FILE_OFFSET_BITS 64
-
 #include <sys/vfs.h>
 #include <errno.h>
 #include <stdio.h>
--- test/misc/tst-statvfs.c
+++ test/misc/tst-statvfs.c
@@ -1,5 +1,3 @@
-#define _FILE_OFFSET_BITS 64
-
 #include <sys/statvfs.h>
 #include <errno.h>
 #include <stdio.h>
