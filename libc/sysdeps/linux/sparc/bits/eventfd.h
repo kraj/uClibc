9fc6da20daeac3f06116630764c36fc4943b1f3a
Khem Raj <raj.khem@gmail.com>
2014-02-20 00:30:18 -0800

    Add eventfd_read() and eventfd_write()
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/sparc/bits/eventfd.h
+++ libc/sysdeps/linux/sparc/bits/eventfd.h
@@ -22,7 +22,7 @@
 /* Flags for eventfd.  */
 enum
   {
-    EFD_SEMAPHORE = 1,
+    EFD_SEMAPHORE = 0x000001,
 #define EFD_SEMAPHORE EFD_SEMAPHORE
     EFD_CLOEXEC = 0x400000,
 #define EFD_CLOEXEC EFD_CLOEXEC
