0550c0c5192e009409d9a23f644419699c9eb500
Vineet Gupta <vgupta@synopsys.com>
2014-07-25 17:39:06 +0530

    ARC/NPTL: Fix __libc_sigaction redefinition with static links
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libpthread/nptl/sysdeps/unix/sysv/linux/arc/pt-__syscall_rt_sigaction.c
+++ libpthread/nptl/sysdeps/unix/sysv/linux/arc/pt-__syscall_rt_sigaction.c
@@ -4,4 +4,10 @@
  * Licensed under the LGPL v2.1 or later, see the file COPYING.LIB in this tarball.
  */
 
-#include <../../../../../../../libc/sysdeps/linux/arc/sigaction.c>
+/*
+ * ARC syscall ABI only has __NR_rt_sigaction, thus vanilla sigaction does
+ * some SA_RESTORER tricks before calling __syscall_rt_sigaction.
+ * However including that file here causes a redefinition of __libc_sigaction
+ * in static links involving pthreads
+ */
+//#include <../../../../../../../libc/sysdeps/linux/arc/sigaction.c>
