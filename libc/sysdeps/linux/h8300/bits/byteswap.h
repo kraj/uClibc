dc19aa547995f92b89449ed9ebb69ea08b2098ca
Yoshinori Sato <ysato@users.sourceforge.jp>
2015-05-24 02:06:25 +0900

    h8300: headers update
    - wire up new kernel
    
    Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libc/sysdeps/linux/h8300/bits/byteswap.h
+++ libc/sysdeps/linux/h8300/bits/byteswap.h
@@ -19,20 +19,7 @@
 #ifndef _ASM_BITS_BYTESWAP_H
 #define _ASM_BITS_BYTESWAP_H 1
 
-#define __bswap_non_constant_32(x) \
-  __extension__						\
-  ({ unsigned int __v;					\
-     __asm__ __volatile__ ("mov.l %0,er0\n\t"		\
-			   "mov.b r0l,r1h\n\t"		\
-			   "mov.b r0h,r1l\n\t"		\
-			   "mov.w r1,e1\n\t"		\
-			   "mov.w e0,r0\n\t"		\
-			   "mov.b r0l,r1h\n\t"		\
-			   "mov.b r0h,r1l\n\t"		\
-			   "mov.l er1,%0"		\
-			   : "=d" (__v)			\
-			   : "0" (x): "er0","er1");	\
-     __v; })
+#define __bswap_non_constant_32(x) __builtin_bswap32(x)
 
 #endif
 
--- libc/sysdeps/linux/h8300/bits/kernel_types.h
+++ libc/sysdeps/linux/h8300/bits/kernel_types.h
@@ -35,6 +35,18 @@ typedef __kernel_dev_t	__kernel_old_dev_t;
 typedef long		__kernel_long_t;
 typedef unsigned long	__kernel_ulong_t;
 
+#define __kernel_long_t		__kernel_long_t
+#define __kernel_ino_t 		__kernel_ino_t
+#define __kernel_mode_t 	__kernel_mode_t
+#define __kernel_pid_t 		__kernel_pid_t
+#define __kernel_ipc_pid_t 	__kernel_ipc_pid_t
+#define __kernel_uid_t 		__kernel_uid_t
+#define __kernel_susecond_t 	__kernel_susecond_t
+#define __kernel_daddr_t 	__kernel_daddr_t
+#define __kernel_uid32_t 	__kernel_uid32_t
+#define __kernel_old_uid_t 	__kernel_old_uid_t
+#define __kernel_old_dev_t 	__kernel_old_dev_t
+
 typedef struct {
 #ifdef __USE_ALL
 	int val[2];
@@ -42,5 +54,6 @@ typedef struct {
 	int __val[2];
 #endif
 } __kernel_fsid_t;
+#define __kernel_fsid_t __kernel_fsid_t
 
 #endif /* _BITS_KERNEL_TYPES_H */
